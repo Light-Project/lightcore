d575e009371b49d22a12229d24a331a20c0a3153
sanpe <1512819690@qq.com>
2022-03-10 05:18:04 +0800

    [sections] add cpuidle entry irq etc.

--- include/compiler/sections.h
+++ include/compiler/sections.h
@@ -5,9 +5,16 @@
 #define __init  __cold  __section(".init.text")
 #define __initdata      __section(".init.data")
 #define __initconst     __section(".init.rodata")
+
+#define __exit  __cold  __section(".exit.text")
 #define __exitdata      __section(".exit.data")
-#define __exit_call     __section(".exit.exitcall")
+#define __exitconst     __section(".exit.rodata")
+
 #define __sched         __section(".text.sched")
+#define __cpuidle       __section(".text.cpuidle")
+#define __entry         __section(".text.entry")
+#define __irq_entry     __section(".text.irqentry")
+#define __softirq_entry __section(".text.softirqentry")
 #define __read_mostly   __section(".data.readmostly")
 
 #endif   /* _COMPILER_SECTIONS_H_ */
