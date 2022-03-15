bf5e6f236c212c25ae991e616a9e61cbb3dc6154
sanpe <1512819690@qq.com>
2022-03-15 08:09:48 +0800

    Maintain: Added kasan check header
    [kasan] Added for the first time
        -- Manual detection for assembly.

---
+++ include/kasan-check.h
@@ -0,0 +1,25 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _KASAN_CHECK_H_
+#define _KASAN_CHECK_H_
+
+#include <types.h>
+
+#ifndef CONFIG_KASAN
+
+static inline bool kasan_check_read(const volatile void *addr, unsigned int size)
+{
+    return true;
+}
+
+static inline bool kasan_check_write(const volatile void *addr, unsigned int size)
+{
+    return true;
+}
+
+#else /* !CONFIG_KASAN */
+
+extern bool kasan_check_read(const volatile void *addr, unsigned int size);
+extern bool kasan_check_write(const volatile void *addr, unsigned int size);
+
+#endif  /* CONFIG_KASAN */
+#endif  /* _KASAN_CHECK_H_ */
