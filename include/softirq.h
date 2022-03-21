e2294499f8d1ef2e4eaf0e32a951159a6de2b30d
John-sanpe <sanpeqf@gmail.com>
2022-03-21 19:26:43 +0800

    Maintain: Fixed a panic caused by softirq
    [softirq] Fixed two issues and added some features
        -- The method of saving interrupts is changed to prevent
            problems that may be caused by hanging in interrupts.
    
        -- Fixed the problem of quantity conversion between IDR and
            bitmap, and added the handling measure of empty ID suspension.
    
        -- Added interrupt safe mode and loop mode, and the function of
            dynamically creating nodes is added.

--- include/softirq.h
+++ include/softirq.h
@@ -5,23 +5,57 @@
 #include <irqtypes.h>
 #include <idr.h>
 
+typedef void (*softirq_entry_t)(void *pdata);
+
+enum timer_flags {
+    __SOFTIRQ_IRQ_SAFE      = 0,
+    __SOFTIRQ_PERIODIC      = 1,
+};
+
+#define SOFTIRQ_IRQ_SAFE    BIT(__SOFTIRQ_IRQ_SAFE)
+#define SOFTIRQ_PERIODIC    BIT(__SOFTIRQ_PERIODIC)
+
+/**
+ * struct softirq - Describe a softirq node
+ * @node: IDR used to assign softirq index number
+ * @name: node name for management
+ * @flags: characteristics flags of nodes
+ * @entry: entry function of soft interrupt
+ * @pdata: parameters of the entry function
+ */
 struct softirq {
     struct idr_node node;
     const char *name;
-    void (*entry)(void *pdata);
+    unsigned long flags;
+    softirq_entry_t entry;
     void *pdata;
 };
 
-#define SOFTIRQ_INITIALIZER(_name, _entry, _pdata) \
-    (struct softirq) { .name = (_name), .entry = (_entry), .pdata = (_pdata) }
+#define SOFTIRQ_INITIALIZER(_name, _entry, _pdata, _flags) \
+    (struct softirq) { .name = (_name), .entry = (_entry), .pdata = (_pdata), .flags = (_flags) }
 
-#define DEFINE_SOFTIRQ(name, entry, pdata) \
-    struct softirq name = SOFTIRQ_INITIALIZER(#name, entry, pdata)
+#define DEFINE_SOFTIRQ(name, entry, pdata, flags) \
+    struct softirq name = SOFTIRQ_INITIALIZER(#name, entry, pdata, flags)
+
+GENERIC_STRUCT_BITOPS(softirq, struct softirq, flags)
+#define softirq_flags_set(ptr, bit)     generic_softirq_flags_set(ptr, bit)
+#define softirq_flags_clr(ptr, bit)     generic_softirq_flags_clr(ptr, bit)
+#define softirq_flags_test(ptr, bit)    generic_softirq_flags_test(ptr, bit)
+
+#define softirq_set_irq_safe(ptr)       softirq_flags_set(ptr, __SOFTIRQ_IRQ_SAFE)
+#define softirq_clr_irq_safe(ptr)       softirq_flags_clr(ptr, __SOFTIRQ_IRQ_SAFE)
+#define softirq_test_irq_safe(ptr)      softirq_flags_test(ptr, __SOFTIRQ_IRQ_SAFE)
+
+#define softirq_set_periodic(ptr)       softirq_flags_set(ptr, __SOFTIRQ_PERIODIC)
+#define softirq_clr_periodic(ptr)       softirq_flags_clr(ptr, __SOFTIRQ_PERIODIC)
+#define softirq_test_periodic(ptr)      softirq_flags_test(ptr, __SOFTIRQ_PERIODIC)
 
 extern state softirq_pending(struct softirq *irq);
-extern state softirq_clear(struct softirq *irq);
+extern void softirq_clear(struct softirq *irq);
 extern state softirq_regsiter(struct softirq *irq);
 extern void softirq_unregister(struct softirq *irq);
+extern struct softirq *softirq_create(const char *name, softirq_entry_t entry, void *pdata, unsigned long flags);
+extern void softirq_destroy(struct softirq *irq);
 extern void __init softirq_init(void);
 
 #endif  /* _SOFTIRQ_H_ */
--- kernel/irq/softirq.c
+++ kernel/irq/softirq.c
@@ -7,32 +7,45 @@
 #define pr_fmt(fmt) MODULE_NAME ": " fmt
 
 #include <linkage.h>
+#include <kmalloc.h>
 #include <softirq.h>
+#include <export.h>
+#include <panic.h>
 
-#define SOFTIRQ_RESTART_NR 16
+#define SOFTIRQ_LIMIT   CONFIG_SOFTIRQ_LIMIT
+#define SOFTIRQ_RETRY   16
+
+static struct kcache *softirq_cache;
 static struct idr *softirq_idr;
 
 asmlinkage __visible __softirq_entry void softirq_handle(void)
 {
-    unsigned int max_restart = SOFTIRQ_RESTART_NR;
+    unsigned int max_restart = SOFTIRQ_RETRY;
     struct irq_cpustat *stat = thiscpu_ptr(&irq_stat);
     unsigned long *pending = stat->softirq_pending;
     unsigned int softirq_bit;
 
 restart:
-    irq_local_enable();
-
-    while ((softirq_bit = find_first_bit(pending, CONFIG_SOFTIRQ_LIMIT)) != ULONG_MAX) {
+    while ((softirq_bit = find_first_bit(pending, SOFTIRQ_LIMIT)) < SOFTIRQ_LIMIT) {
         struct softirq *softirq;
 
-        bit_clr(pending, softirq_bit);
-        softirq = idr_find(softirq_idr, softirq_bit);
-        softirq->entry(softirq->pdata);
+        softirq = idr_find(softirq_idr, softirq_bit + 1);
+        if (BUG_ON(!softirq))
+            continue;
+
+        if (!softirq_test_periodic(softirq))
+            bit_clr(pending, softirq_bit);
+
+        if (!softirq_test_irq_safe(softirq))
+            softirq->entry(softirq->pdata);
+        else {
+            irq_local_enable();
+            softirq->entry(softirq->pdata);
+            irq_local_disable();
+        }
     }
 
-    irq_local_disable();
-
-    if (!bitmap_empty(stat->softirq_pending, CONFIG_SOFTIRQ_LIMIT)) {
+    if (!bitmap_empty(stat->softirq_pending, SOFTIRQ_LIMIT)) {
         if (max_restart--)
             goto restart;
     }
@@ -51,13 +64,13 @@ void noinline softirq_exit(void)
 static inline void raise_softirq(unsigned int irqnr)
 {
     struct irq_cpustat *stat = thiscpu_ptr(&irq_stat);
-    bit_set(stat->softirq_pending, irqnr);
+    bit_set(stat->softirq_pending, irqnr - 1);
 }
 
 static inline void decline_softirq(unsigned int irqnr)
 {
     struct irq_cpustat *stat = thiscpu_ptr(&irq_stat);
-    bit_clr(stat->softirq_pending, irqnr);
+    bit_clr(stat->softirq_pending, irqnr - 1);
 }
 
 /**
@@ -66,23 +79,35 @@ static inline void decline_softirq(unsigned int irqnr)
  */
 state softirq_pending(struct softirq *irq)
 {
-    irq_local_disable();
+    irqflags_t irqflags;
+
+    if (BUG_ON(!irq || !irq->entry))
+        return -EINVAL;
+
+    irqflags = irq_local_save();
     raise_softirq(irq->node.index);
-    irq_local_enable();
+    irq_local_restore(irqflags);
+
     return -ENOERR;
 }
+EXPORT_SYMBOL(softirq_pending);
 
 /**
  * softirq_clear - clear a softirq
  * @irq: the softirq to clear
  */
-state softirq_clear(struct softirq *irq)
+void softirq_clear(struct softirq *irq)
 {
-    irq_local_disable();
+    irqflags_t irqflags;
+
+    if (BUG_ON(!irq))
+        return;
+
+    irqflags = irq_local_save();
     decline_softirq(irq->node.index);
-    irq_local_enable();
-    return -ENOERR;
+    irq_local_restore(irqflags);
 }
+EXPORT_SYMBOL(softirq_clear);
 
 /**
  * softirq_regsiter - regsiter a softirq
@@ -92,12 +117,16 @@ state softirq_regsiter(struct softirq *irq)
 {
     unsigned long id;
 
-    id = idr_node_alloc(softirq_idr, &irq->node, irq);
+    if (BUG_ON(!irq || !irq->entry))
+        return -EINVAL;
+
+    id = idr_node_alloc_cyclic_max(softirq_idr, &irq->node, irq, SOFTIRQ_LIMIT);
     if (id == IDR_NONE)
         return -ENOMEM;
 
     return -ENOERR;
 }
+EXPORT_SYMBOL(softirq_regsiter);
 
 /**
  * softirq_unregister - unregsiter a softirq
@@ -105,10 +134,48 @@ state softirq_regsiter(struct softirq *irq)
  */
 void softirq_unregister(struct softirq *irq)
 {
+    softirq_clear(irq);
     idr_node_free(softirq_idr, irq->node.index);
 }
+EXPORT_SYMBOL(softirq_unregister);
+
+/**
+ * softirq_create - Create a softirq node
+ * @name: the name of softirq
+ * @entry: the softirq handle entry
+ * @pdata: handle entry pdata
+ * @flags: softirq flags
+ */
+struct softirq *softirq_create(const char *name, softirq_entry_t entry, void *pdata, unsigned long flags)
+{
+    struct softirq *irq;
+
+    irq = kcache_zalloc(softirq_cache, GFP_KERNEL);
+    if (unlikely(!irq))
+        return NULL;
+
+    irq->name = name;
+    irq->entry = entry;
+    irq->pdata = pdata;
+    irq->flags = flags;
+
+    return irq;
+}
+EXPORT_SYMBOL(softirq_create);
+
+/**
+ * softirq_destroy - Destroy a softirq node
+ * @irq: the softirq to destroy
+ */
+void softirq_destroy(struct softirq *irq)
+{
+    softirq_unregister(irq);
+    kcache_free(softirq_cache, irq);
+}
+EXPORT_SYMBOL(softirq_destroy);
 
 void __init softirq_init(void)
 {
+    softirq_cache = KCACHE_CREATE(struct softirq, KCACHE_PANIC);
     softirq_idr = idr_create(0);
 }
