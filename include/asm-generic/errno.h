1ea1aaf7534943085f2310cac9dc3c0b578ff5b8
John-sanpe <sanpeqf@gmail.com>
2022-03-29 19:06:24 +0800

    Maintain: Redesigned the header file path of errno.
    [state] Differentiated different functional parts of errno.
        -- Use the four level differentiation method to
            accurately control the error code requirements
            of kernel users on different platforms.

---
+++ include/asm-generic/errno.h
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _ASM_GENERIC_ERRNO_H_
+#define _ASM_GENERIC_ERRNO_H_
+
+#include <lightcore/asm/errno.h>
+
+#endif  /* _ASM_GENERIC_ERRNO_H_ */
