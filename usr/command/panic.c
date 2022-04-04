0e290d0b3af498d36aef0c1c731005f3d8570003
John-sanpe <sanpeqf@gmail.com>
2022-04-05 01:22:05 +0800

    Function: Added the function of triggering crash.
    [panic-cmd] Add function.
        -- Added panic support for warn and bug.

--- usr/command/panic.c
+++ usr/command/panic.c
@@ -7,18 +7,69 @@
 #include <kshell.h>
 #include <panic.h>
 
+enum panic_type {
+    PANIC_WARN      = 0,
+    PANIC_BUG       = 1,
+    PANIC_PANIC     = 2,
+};
+
 static void usage(void)
 {
-    kshell_printf("usage: panic \"message\"\n");
+    kshell_printf("usage: panic [option] \"message\"\n");
+    kshell_printf("\t-w  manually trigger a warn\n");
+    kshell_printf("\t-b  manually trigger a bug\n");
+    kshell_printf("\t-p  manually trigger a bug\n");
+    kshell_printf("\t-h  display this message\n");
 }
 
 static state panic_main(int argc, char *argv[])
 {
-    if (argc < 2) {
-        usage();
-        return -EINVAL;
+    enum panic_type type = PANIC_PANIC;
+    unsigned int count;
+
+    for (count = 1; count < argc; ++count) {
+        char *para = argv[count];
+
+        if (para[0] != '-')
+            break;
+
+        switch (para[1]) {
+            case 'w':
+                type = PANIC_WARN;
+                break;
+
+            case 'b':
+                type = PANIC_BUG;
+                break;
+
+            case 'p':
+                type = PANIC_PANIC;
+                break;
+
+            case 'h': default:
+                goto usage;
+        }
     }
-    panic("%s", argv[1]);
+
+    switch (type) {
+        case PANIC_WARN:
+            WARN();
+            break;
+
+        case PANIC_BUG:
+            BUG();
+            break;
+
+        case PANIC_PANIC:
+            panic("%s", argv[1]);
+            unreachable();
+    }
+
+    return -ENOERR;
+
+usage:
+    usage();
+    return -EINVAL;
 }
 
 static struct kshell_command panic_cmd = {
