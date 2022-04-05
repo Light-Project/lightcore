ceb2f35d4ee7b5d91940d87a9753d901bac4759a
John-sanpe <sanpeqf@gmail.com>
2022-04-06 06:35:43 +0800

    Function: Add resource framework for the first time.
    [resource] Initial construction.
        -- Although we have been using this structure before,
            we do not have a unified management framework.

--- include/resource.h
+++ include/resource.h
@@ -2,35 +2,59 @@
 #ifndef _RESOURCE_H_
 #define _RESOURCE_H_
 
-#include <list.h>
 #include <bits.h>
+#include <list.h>
+#include <rbtree.h>
 
 enum resource_type {
+    /* Resource types */
     RESOURCE_NONE       = BIT_SHIFT(0, 0),
     RESOURCE_PMIO       = BIT_SHIFT(0, 1),
     RESOURCE_MMIO       = BIT_SHIFT(0, 2),
     RESOURCE_IRQ        = BIT_SHIFT(0, 3),
     RESOURCE_DMA        = BIT_SHIFT(0, 4),
     RESOURCE_TYPE       = BIT_RANGE(7, 0),
+
+    /* Resource extended flags */
     RESOURCE_MEM64      = BIT_SHIFT(8, 1),
+    RESOURCE_DISABLED   = BIT_SHIFT(8, 2),
+    RESOURCE_UNSET      = BIT_SHIFT(8, 4),
+    RESOURCE_BUSY       = BIT_SHIFT(8, 8),
+
+    /* PCI Resource */
+    RESOURCE_PCI_ROM_ENABLE = BIT_SHIFT(16, 1),
+    RESOURCE_PCI_ROM_SHADOW = BIT_SHIFT(16, 2),
 };
 
 struct resource {
-    struct list_head list;
+    struct list_head siblings;
+    struct list_head child;
+    struct resource *parent;
+    struct rb_node sort;
     const char *name;
     resource_size_t start;
     resource_size_t size;
     enum resource_type type;
 };
 
-static inline resource_size_t resource_end(struct resource *resource)
+#define rb_to_resource(io) \
+    rb_entry(io, struct resource, sort)
+
+#define rb_to_resource_safe(io) \
+    rb_entry_safe(io, struct resource, sort)
+
+static inline resource_size_t resource_end(const struct resource *resource)
 {
     return (resource->start + resource->size) - 1;
 }
 
-static inline enum resource_type resource_type(struct resource *resource)
+static inline enum resource_type resource_type(const struct resource *resource)
 {
     return resource->type & RESOURCE_TYPE;
 }
+
+extern struct resource *resource_register_conflict(struct resource *parent, struct resource *res);
+extern state resource_register(struct resource *parent, struct resource *res);
+extern state resource_unregister(struct resource *res);
 
 #endif  /* _RESOURCE_H_ */
--- kernel/resource.c
+++ kernel/resource.c
@@ -3,31 +3,86 @@
  * Copyright(c) 2021 Sanpe <sanpeqf@gmail.com>
  */
 
-#include <memory.h>
-#include <initcall.h>
+#define MODULE_NAME "res"
+#define pr_fmt(fmt) MODULE_NAME ": " fmt
+
+#include <spinlock.h>
 #include <resource.h>
+#include <panic.h>
 #include <export.h>
 
-static LIST_HEAD(resource_list);
+static RB_ROOT(resource_sort);
+static LIST_HEAD(resource_root);
+static SPIN_LOCK(resource_lock);
 
-state resource_register(struct resource *resource)
+static long resource_rb_find(const struct rb_node *rb, const void *key)
 {
-    if (!resource)
-        return -EINVAL;
+    const struct resource *node = rb_to_resource(rb);
+    const struct resource *cmp = key;
 
-    list_add_prev(&resource_list, &resource->list);
-    return -ENOERR;
+    if (resource_type(node) == resource_type(cmp) && !(
+        resource_end(cmp) < node->start || resource_end(node) < cmp->start))
+        return 0;
+
+    if (node->type != cmp->type)
+        return node->type > cmp->type ? -1 : 1;
+
+    if (node->parent != cmp->parent)
+        return node->parent > cmp->parent ? -1 : 1;
+
+    if (node->start != cmp->start)
+        return node->start > cmp->start ? -1 : 1;
+
+    if (node->size != cmp->size)
+        return node->size > cmp->size ? -1 : 1;
+
+    return LONG_MIN;
+}
+
+struct resource *resource_register_conflict(struct resource *parent, struct resource *res)
+{
+    struct rb_node *node, *rbparent, **rblink;
+    struct resource *find;
+
+    spin_lock(&resource_lock);
+
+    node = rb_find_last(&resource_sort, res, resource_rb_find, &rbparent, &rblink);
+    if (unlikely(find = rb_to_resource_safe(node)))
+        goto exit;
+
+    res->parent = parent;
+    list_head_init(&res->child);
+    list_add(&parent->child, &res->siblings);
+    rb_insert_node(&resource_sort, rbparent, rblink, &res->sort);
+
+exit:
+    spin_unlock(&resource_lock);
+    return find;
+}
+EXPORT_SYMBOL(resource_register_conflict);
+
+state resource_register(struct resource *parent, struct resource *res)
+{
+    struct resource *conflict;
+    conflict = resource_register_conflict(parent, res);
+    return conflict ? -EBUSY : -ENOERR;
 }
 EXPORT_SYMBOL(resource_register);
 
-void resource_unregister(struct resource *resource)
+state resource_unregister(struct resource *res)
 {
-    list_del(&resource->list);
+    state retval = -ENOERR;
+
+    spin_lock(&resource_lock);
+
+    if (!list_check_empty(&res->child))
+        retval = -EBUSY;
+    else {
+        list_del(&res->siblings);
+        rb_delete(&resource_sort, &res->sort);
+    }
+
+    spin_unlock(&resource_lock);
+    return retval;
 }
 EXPORT_SYMBOL(resource_unregister);
-
-static state resource_init(void)
-{
-    return -ENOERR;
-}
-fs_initcall(resource_init);
