85a0c2d008b92ab81c50513fd278a8a6360f0808
John-sanpe <sanpeqf@gmail.com>
2022-04-16 07:55:58 +0800

    Supplement: Added true and false directives.
    [true/false] Add this instruction for the first time.
        -- This instruction is used to return a specific state
            and is mainly used as a judgment statement in a specific case.
    
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ usr/command/false.c
@@ -0,0 +1,7 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#define EXIT_STATUS EPERM
+#include "true.c"
---
+++ usr/command/true.c
@@ -0,0 +1,46 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <kshell.h>
+#include <string.h>
+#include <initcall.h>
+
+#ifndef EXIT_STATUS
+# define EXIT_STATUS ENOERR
+#endif
+
+#if EXIT_STATUS == ENOERR
+# define PROGRAM_NAME true
+# define PROGRAM_DESC "Exit code indicating success"
+#else
+# define PROGRAM_NAME false
+# define PROGRAM_DESC "Exit code indicating failure"
+#endif
+
+static void usage(void)
+{
+    kshell_printf("usage: " __stringify(PROGRAM_NAME)
+                  " [ignored command line arguments]\n");
+}
+
+static state __PASTE(PROGRAM_NAME, main)(int argc, char *argv[])
+{
+    if (argc != 1)
+        usage();
+
+    return -EXIT_STATUS;
+}
+
+static struct kshell_command watch_cmd = {
+    .name = __stringify(PROGRAM_NAME),
+    .desc = "execute a program periodically",
+    .exec = __PASTE(PROGRAM_NAME, main),
+};
+
+static state watch_init(void)
+{
+    return kshell_register(&watch_cmd);
+}
+kshell_initcall(watch_init);
