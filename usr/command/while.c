27ba237983c954965b874a58c3568fd662ea9d58
John-sanpe <sanpeqf@gmail.com>
2022-05-08 09:54:20 +0800

    Function: Added the while statement of kshell.
    [while] Function initialization addition.
        -- Why do we need to add while when we have the for syntax:
            The difference between it and the for syntax is that its
            parameters are dynamically detected, so we can use it in
            situations where some dynamic judgments are used.
    
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ usr/command/while.c
@@ -0,0 +1,62 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <kshell.h>
+#include <initcall.h>
+#include <string.h>
+
+static void usage(void)
+{
+    kshell_printf("usage: while [option] 'condition' {commands}\n");
+    kshell_printf("\t-h  display this message\n");
+}
+
+static state while_main(int argc, char *argv[])
+{
+    unsigned int count;
+    state retval = -ENOERR;
+
+    for (count = 1; count < argc; ++count) {
+        char *para = argv[count];
+
+        if (para[0] != '-')
+            break;
+
+        switch (para[1]) {
+            case 'h': default:
+                goto usage;
+        }
+    }
+
+    if (argc < count + 2)
+        goto usage;
+
+    while (!kshell_system(argv[count + 0])) {
+        if (kshell_ctrlc())
+            break;
+        retval = kshell_system(argv[count + 1]);
+        if (retval && retval != -EAGAIN)
+            break;
+        retval = -ENOERR;
+    }
+
+    return retval;
+
+usage:
+    usage();
+    return -EINVAL;
+}
+
+static struct kshell_command while_cmd = {
+    .name = "while",
+    .desc = "circulates execute commands",
+    .exec = while_main,
+};
+
+static state while_init(void)
+{
+    return kshell_register(&while_cmd);
+}
+kshell_initcall(while_init);
