6b243dd8aa34df7c94bc908b95393e398a65f975
John-sanpe <sanpeqf@gmail.com>
2022-05-14 10:27:45 +0800

    Function: The first design of kernel lockless queue.
    [kfifo] Functional addition.
        -- We have designed a new kernel queue, which is lockless
            in the case of one producer and one consumer. What's more,
            it supports the record mode, which can automatically record
            the length of data put in each time.
    
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

--- include/kfifo.h
+++ include/kfifo.h
@@ -2,27 +2,384 @@
 #ifndef _KFIFO_H_
 #define _KFIFO_H_
 
-#include <types.h>
+#include <kernel.h>
 
-struct kfifo{
-    char *buffer;
-    size_t in, out;
-    size_t size;
+struct kfifo {
+    unsigned long in;
+    unsigned long out;
+    unsigned long mask;
+    unsigned long esize;
+    void *data;
 };
 
-static inline size_t fifo_len(struct kfifo *fifo)
-{
-    return fifo->in - fifo->out;
+/**
+ * GENERIC_KFIFO - define a generic kfifo structure.
+ * @datatype: kfifo data type.
+ * @ptrtype: kfifo pointer containing data.
+ * @rsize: kfifo record size.
+ */
+#define GENERIC_KFIFO(datatype, ptrtype, rsize)     \
+    union {                                         \
+        struct kfifo kfifo;                         \
+        datatype *data;                             \
+        const datatype *cdata;                      \
+        ptrtype *ptr;                               \
+        const ptrtype *cptr;                        \
+        char (*rectype)[rsize];                     \
+    }
+
+/**
+ * BODY_KFIFO - generate the body of normal kfifo.
+ * @type: kfifo contains the type of data.
+ * @ptype: kfifo pointer containing data.
+ * @size: kfifo buffer size.
+ * @rsize: kfifo record size.
+ */
+#define BODY_KFIFO(type, ptype, size, rsize) {      \
+    GENERIC_KFIFO(type, ptype, rsize);              \
+    type buff[((size < 2) ||                        \
+        (size & (size - 1))) ? -1 : size];          \
 }
 
-static inline void fifo_reset(struct kfifo *fifo)
-{
-    fifo->in = fifo->out = 0;
+/**
+ * BODY_KFIFO_DYNAMIC - generate the body of dynamic kfifo.
+ * @type: kfifo contains the type of data.
+ * @ptype: kfifo pointer containing data.
+ * @rsize: kfifo record size.
+ */
+#define BODY_KFIFO_DYNAMIC(type, ptype, rsize) {    \
+    GENERIC_KFIFO(type, ptype, rsize);              \
+    type buff[0];                                   \
 }
 
-void kfifo_read(struct kfifo *fifo, uint8_t *buff, size_t size);
-void kfifo_write(struct kfifo *fifo, uint8_t *buff, size_t size);
-struct kfifo *kfifo_alloc(size_t size);
-void kfifo_free(struct kfifo *fifo);
+/**
+ * KFIFO_INIT - initialize normal kfifo in compound literals.
+ * @name: the name of kfifo to init.
+ */
+#define KFIFO_INIT(name) (typeof(name)) {           \
+    .kfifo = {                                      \
+        .in = 0, .out = 0,                          \
+        .esize = sizeof(*(name).buff),              \
+        .mask = ARRAY_SIZE((name).buff) - 1,        \
+        .data = (name).buff,                        \
+    },                                              \
+}
 
-#endif
+/**
+ * KFIFO_DYNAMIC_INIT - initialize dynamic kfifo in compound literals.
+ * @name: the name of kfifo to init.
+ */
+#define KFIFO_DYNAMIC_INIT(name) (typeof(name)) {{  \
+    .kfifo = {                                      \
+        .in = 0, .out = 0, .mask = 0, .data = NULL, \
+        .esize = sizeof(*(name).buff),              \
+    },                                              \
+}}
+
+/**
+ * STRUCT_KFIFO - generate a normal kfifo structure.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ */
+#define STRUCT_KFIFO(type, size) \
+    struct BODY_KFIFO(type, type, size, 0)
+
+/**
+ * STRUCT_KFIFO_RECORD - generate a record kfifo structure.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ * @record: kfifo record size.
+ */
+#define STRUCT_KFIFO_RECORD(type, size, record) \
+    struct BODY_KFIFO(type, type, size, record)
+
+/**
+ * STRUCT_KFIFO_DYNAMIC - generate a dynamic kfifo structure.
+ * @type: kfifo contains the type of data.
+ */
+#define STRUCT_KFIFO_DYNAMIC(type) \
+    struct BODY_KFIFO_DYNAMIC(type, type, 0)
+
+/**
+ * STRUCT_KFIFO_DYNAMIC_RECORD - generate a dynamic record kfifo structure.
+ * @type: kfifo contains the type of data.
+ * @record: kfifo record size.
+ */
+#define STRUCT_KFIFO_DYNAMIC_RECORD(type, record) \
+    struct BODY_KFIFO_DYNAMIC(type, type, record)
+
+/**
+ * DECLARE_KFIFO - declare a normal kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ */
+#define DECLARE_KFIFO(name, type, size) \
+    STRUCT_KFIFO(type, size) name
+
+/**
+ * DECLARE_KFIFO_RECORD - declare a record kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ * @record: kfifo record size.
+ */
+#define DECLARE_KFIFO_RECORD(name, type, size, record) \
+    STRUCT_KFIFO_RECORD(type, size, record) name
+
+/**
+ * DECLARE_KFIFO_DYNAMIC - declare a dynamic kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ */
+#define DECLARE_KFIFO_DYNAMIC(name, type) \
+    STRUCT_KFIFO_DYNAMIC(type) name
+
+/**
+ * DECLARE_KFIFO_DYNAMIC_RECORD - declare a dynamic record kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @record: kfifo record size.
+ */
+#define DECLARE_KFIFO_DYNAMIC_RECORD(name, type, record) \
+    STRUCT_KFIFO_DYNAMIC_RECORD(type, record) name
+
+/**
+ * DEFINE_KFIFO - define a normal kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ */
+#define DEFINE_KFIFO(name, type, size) \
+    DECLARE_KFIFO(name, type, size) = KFIFO_INIT(name)
+
+/**
+ * DEFINE_KFIFO - define a record kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @size: kfifo buffer size.
+ * @record: kfifo record size.
+ */
+#define DEFINE_KFIFO_RECORD(name, type, size, record) \
+    DECLARE_KFIFO_RECORD(name, type, size, record) = KFIFO_INIT(name)
+
+/**
+ * DEFINE_KFIFO_DYNAMIC - define a dynamic kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ */
+#define DEFINE_KFIFO_DYNAMIC(name, type) \
+    DECLARE_KFIFO_DYNAMIC(name, type) = KFIFO_DYNAMIC_INIT(name)
+
+/**
+ * DEFINE_KFIFO_DYNAMIC_RECORD - declare define dynamic record kfifo structure.
+ * @name: name of kfifo structure to declare.
+ * @type: kfifo contains the type of data.
+ * @record: kfifo record size.
+ */
+#define DEFINE_KFIFO_DYNAMIC_RECORD(name, type, record) \
+    DECLARE_KFIFO_DYNAMIC_RECORD(name, type, record) = KFIFO_DYNAMIC_INIT(name)
+
+/**
+ * kfifo_initialized - check if the fifo is initialized.
+ * @fifo: pointer of the fifo to check.
+ */
+#define kfifo_initialized(fifo) ((fifo)->kfifo.mask)
+
+/**
+ * kfifo_recsize - get the size of the record length field.
+ * @fifo: pointer of the fifo to get recsize.
+ */
+#define kfifo_recsize(fifo)	(sizeof(*(fifo)->rectype))
+
+/**
+ * kfifo_size - get the size of the element managed by the fifo.
+ * @fifo: pointer of the fifo to get size.
+ */
+#define kfifo_size(fifo) ((fifo)->kfifo.mask + 1)
+
+/**
+ * kfifo_esize - get the size of the fifo in elements.
+ * @fifo: pointer of the fifo to get size.
+ */
+#define kfifo_esize(fifo) ((fifo)->kfifo.esize)
+
+/**
+ * kfifo_reset - reset fifo state.
+ * @fifo: the fifo to reset.
+ */
+#define kfifo_reset(fifo) do {                      \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    tmp->kfifo.out = tmp->kfifo.in = 0;             \
+} while (0)
+
+/**
+ * kfifo_homing - homing unread valid data length.
+ * @fifo: the fifo to homing.
+ */
+#define kfifo_homing(fifo) do {                     \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    tmp->kfifo.out = tmp->kfifo.in;                 \
+} while (0)
+
+/**
+ * kfifo_len - get the valid data length in kfifo.
+ * @fifo: the fifo to get.
+ */
+#define kfifo_len(fifo) ({                          \
+    typeof((fifo) + 1) tlen = (fifo);               \
+    tlen->kfifo.in - tlen->kfifo.out;               \
+})
+
+/**
+ * kfifo_check_empty - check whether kfifo is empty.
+ * @fifo: the fifo to check.
+ */
+#define kfifo_check_empty(fifo) ({                  \
+    typeof((fifo) + 1) ttmp = (fifo);               \
+    ttmp->kfifo.in == ttmp->kfifo.out;              \
+})
+
+/**
+ * kfifo_check_full - check whether kfifo is full.
+ * @fifo: the fifo to check.
+ */
+#define kfifo_check_full(fifo) ({                   \
+    typeof((fifo) + 1) ttmp = (fifo);               \
+    kfifo_len(ttmp) > ttmp->kfifo.mask;             \
+})
+
+/**
+ * kfifo_check_dynamic - check whether kfifo is dynamic.
+ * @fifo: the fifo to check.
+ */
+#define kfifo_check_dynamic(fifo) (                 \
+    !sizeof(fifo->buff)                             \
+)
+
+/**
+ * kfifo_alloc - dynamically allocate buffer to kfifo.
+ * @fifo: the fifo to allocate buffer.
+ * @size: size of buffer.
+ * @flags: page flags.
+ */
+#define kfifo_alloc(fifo, size, flags) ({           \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    kfifo_check_dynamic(tmp) ?                      \
+    kfifo_dynamic_alloc(&tmp->kfifo,                \
+    sizeof(*tmp->data), size, flags) :              \
+    -EINVAL;                                        \
+})
+
+/**
+ * kfifo_free - dynamically free buffer to kfifo.
+ * @fifo: the fifo to free buffer.
+ */
+#define kfifo_free(fifo) ({                         \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    kfifo_check_dynamic(tmp) ?                      \
+    kfifo_dynamic_free(&tmp->kfifo) :               \
+    -EINVAL;                                        \
+})
+
+/**
+ * kfifo_get - get an object from kfifo.
+ * @fifo: the kfifo to get object out.
+ * @value: object to get.
+ */
+#define kfifo_get(fifo, value) ({                   \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    typeof(tmp->ptr) tvalue = (value);              \
+    struct kfifo *kfifo = &tmp->kfifo;              \
+    unsigned long recsize = sizeof(*tmp->rectype);  \
+    unsigned long retval;                           \
+    if (recsize)                                    \
+        retval = kfifo_out_record(kfifo,            \
+                tvalue, sizeof(*tvalue), recsize);  \
+    else {                                          \
+        retval = !kfifo_check_empty(tmp);           \
+        if (retval) {                               \
+            *(typeof(tmp->data)) tvalue =           \
+            (kfifo_check_dynamic(tmp) ?             \
+            ((typeof(tmp->data)) kfifo->data) :     \
+            (tmp->buff))                            \
+            [kfifo->out & tmp->kfifo.mask];         \
+            smp_wmb();                              \
+            ++kfifo->out;                           \
+        }                                           \
+    }                                               \
+    retval;                                         \
+})
+
+/**
+ * kfifo_put - put an object into kfifo.
+ * @fifo: the kfifo to put object in.
+ * @value: object to put.
+ */
+#define kfifo_put(fifo, value) ({                   \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    typeof(*tmp->cdata) tvalue = (value);           \
+    struct kfifo *kfifo = &tmp->kfifo;              \
+    unsigned long recsize = sizeof(*tmp->rectype);  \
+    unsigned long retval;                           \
+    if (recsize)                                    \
+        retval = kfifo_in_record(kfifo,             \
+                &tvalue, sizeof(tvalue), recsize);  \
+    else {                                          \
+        retval = !kfifo_check_full(tmp);            \
+        if (retval) {                               \
+            (kfifo_check_dynamic(tmp) ?             \
+            ((typeof(tmp->data)) kfifo->data) :     \
+            (tmp->buff))                            \
+            [kfifo->in & tmp->kfifo.mask] =         \
+            *(typeof(tmp->data)) &tvalue;           \
+            ++kfifo->in;                            \
+        }                                           \
+    }                                               \
+    retval;                                         \
+})
+
+/**
+ * kfifo_out - copy continuous data from kfifo.
+ * @fifo: the kfifo to copy data out.
+ * @buff: the buffer to copy data in.
+ * @len: number of continuously copied objects.
+ */
+#define kfifo_out(fifo, buff, len) ({               \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    typeof(tmp->ptr) tbuff = (buff);                \
+    struct kfifo *kfifo = &tmp->kfifo;              \
+    unsigned long tlen = (len);                     \
+    unsigned long recsize = sizeof(*tmp->rectype);  \
+    (recsize) ?                                     \
+    kfifo_out_record(kfifo, tbuff, tlen, recsize) : \
+    kfifo_out_flat(kfifo, tbuff, tlen);             \
+})
+
+/**
+ * kfifo_in - copy continuous data into kfifo.
+ * @fifo: the kfifo to copy data in.
+ * @buff: the buffer to copy data out.
+ * @len: number of continuously copied objects.
+ */
+#define kfifo_in(fifo, buff, len) ({                \
+    typeof((fifo) + 1) tmp = (fifo);                \
+    typeof(tmp->cptr) tbuff = (buff);               \
+    struct kfifo *kfifo = &tmp->kfifo;              \
+    unsigned long tlen = (len);                     \
+    unsigned long recsize = sizeof(*tmp->rectype);  \
+    (recsize) ?                                     \
+    kfifo_in_record(kfifo, tbuff, tlen, recsize) :  \
+    kfifo_in_flat(kfifo, tbuff, tlen);              \
+})
+
+extern unsigned long kfifo_peek_flat(struct kfifo *kfifo, void *buff, unsigned long len);
+extern unsigned long kfifo_out_flat(struct kfifo *kfifo, void *buff, unsigned long len);
+extern unsigned long kfifo_in_flat(struct kfifo *kfifo, const void *buff, unsigned long len);
+extern unsigned long kfifo_peek_record(struct kfifo *kfifo, void *buff, unsigned long len, unsigned long record);
+extern unsigned long kfifo_out_record(struct kfifo *kfifo, void *buff, unsigned long len, unsigned long record);
+extern unsigned long kfifo_in_record(struct kfifo *kfifo, const void *buff, unsigned long len, unsigned long record);
+extern state kfifo_dynamic_alloc(struct kfifo *kfifo, size_t esize, size_t size, gfp_t flags);
+extern void kfifo_dynamic_free(struct kfifo *kfifo);
+
+#endif  /* _KFIFO_H_ */
--- lib/kfifo.c
+++ lib/kfifo.c
@@ -1,66 +1,219 @@
 /* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Copyright(c) 2021 Sanpe <sanpeqf@gmail.com>
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
  */
 
-#include <kernel.h>
-#include <string.h>
 #include <kmalloc.h>
 #include <kfifo.h>
-#include <spinlock.h>
+#include <log2.h>
+#include <string.h>
 #include <export.h>
 
-void kfifo_read(struct kfifo *fifo, uint8_t *buff, size_t size)
+#define KFIFO_GENERIC_COPY(copy1, copy2, fold1, fold2) do { \
+    unsigned long size = kfifo->mask + 1;                   \
+    unsigned long esize = kfifo->esize;                     \
+    unsigned long llen;                                     \
+                                                            \
+    offset &= kfifo->mask;                                  \
+    if (esize != 1) {                                       \
+        offset *= esize;                                    \
+        size *= esize;                                      \
+        len *= esize;                                       \
+    }                                                       \
+                                                            \
+    llen = min(len, size - offset);                         \
+    memcpy(copy1, copy2, llen);                             \
+    memcpy(fold1, fold2, len - llen);                       \
+    smp_wmb();                                              \
+} while (0)
+
+static __always_inline void
+kfifo_out_copy(struct kfifo *kfifo, void *buff, unsigned long len, unsigned long offset)
 {
-    size_t rsize; /* fold back length */
-
-    if(!fifo || !buff)
-        return;
-
-    size = min(size, fifo->in - fifo->out);
-    rsize = min(size, (fifo->size - fifo->out) & (fifo->size - 1));
-    memcpy(buff, fifo->buffer + (fifo->out & (fifo->size - 1)), rsize);
-    memcpy(buff + rsize, fifo->buffer, size - rsize);
-    fifo->out += size;
+    KFIFO_GENERIC_COPY(
+        buff, kfifo->data + offset,
+        buff + llen, kfifo->data
+    );
 }
-EXPORT_SYMBOL(kfifo_read);
 
-void kfifo_write(struct kfifo *fifo, uint8_t *buff, size_t size)
+static __always_inline void
+kfifo_in_copy(struct kfifo *kfifo, const void *buff, unsigned long len, unsigned long offset)
 {
-    size_t rsize; /* fold back length */
-
-    if(!fifo || !buff)
-        return;
-
-    size = min(size, fifo->size - fifo->in + fifo->out);
-    rsize = min(size, fifo->size - (fifo->out & (fifo->size - 1)));
-    memcpy(fifo->buffer + (fifo->in & (fifo->size - 1)), buff, rsize);
-	memcpy(fifo->buffer, buff + rsize, size - rsize);
-    fifo->in += size;
+    KFIFO_GENERIC_COPY(
+        kfifo->data + offset, buff,
+        kfifo->data, buff + llen
+    );
 }
-EXPORT_SYMBOL(kfifo_write);
 
-struct kfifo *kfifo_alloc(size_t size)
+static __always_inline unsigned long
+kfifo_out_peek(struct kfifo *kfifo, unsigned long recsize)
 {
-    struct kfifo *fifo;
+    unsigned long mask = kfifo->mask;
+    unsigned long offset = kfifo->out;
+    unsigned long length = 0;
+    uint8_t *data = kfifo->data;
 
-    if (!size)
-        return NULL;
+    if (kfifo->esize != 1) {
+        offset *= kfifo->esize;
+        mask *= kfifo->esize;
+        mask += kfifo->esize - 1;
+    }
 
-    fifo = kmalloc(sizeof(struct kfifo) + size, GFP_KERNEL);
-    if (!fifo)
-        return NULL;
+    while (recsize--) {
+        length <<= BITS_PER_U8;
+        length |= data[offset & mask];
+        offset += kfifo->esize;
+    }
 
-    fifo->buffer = (char *)fifo + sizeof(*fifo);
-    fifo->size = size;
-    fifo_reset(fifo);
-
-    return fifo;
+    return length;
 }
-EXPORT_SYMBOL(kfifo_alloc);
 
-void kfifo_free(struct kfifo *fifo)
+static __always_inline void
+kfifo_in_poke(struct kfifo *kfifo, unsigned long len, unsigned long recsize)
 {
-    kfree(fifo);
+    unsigned long mask = kfifo->mask;
+    unsigned long offset = kfifo->out;
+    uint8_t *data = kfifo->data;
+
+    if (kfifo->esize != 1) {
+        offset *= kfifo->esize;
+        mask *= kfifo->esize;
+        mask += kfifo->esize - 1;
+    }
+
+    while (recsize--) {
+        data[offset & mask] = (uint8_t)len;
+        offset += kfifo->esize;
+        len >>= BITS_PER_U8;
+    }
 }
-EXPORT_SYMBOL(kfifo_free);
+
+static inline bool kfifo_empty(struct kfifo *kfifo)
+{
+    return kfifo->in == kfifo->out;
+}
+
+static inline unsigned long kfifo_valid(struct kfifo *kfifo)
+{
+    return kfifo->in - kfifo->out;
+}
+
+static inline unsigned long kfifo_unused(struct kfifo *kfifo)
+{
+    return (kfifo->mask + 1) - (kfifo->in - kfifo->out);
+}
+
+unsigned long kfifo_peek_flat(struct kfifo *kfifo, void *buff, unsigned long len)
+{
+    unsigned long valid;
+
+    valid = kfifo_valid(kfifo);
+    min_adj(len, valid);
+    kfifo_out_copy(kfifo, buff, len, kfifo->out);
+
+    return len;
+}
+EXPORT_SYMBOL(kfifo_peek_flat);
+
+unsigned long kfifo_out_flat(struct kfifo *kfifo, void *buff, unsigned long len)
+{
+    unsigned long llen;
+
+    llen = kfifo_peek_flat(kfifo, buff, len);
+    kfifo->out += llen;
+
+    return llen;
+}
+EXPORT_SYMBOL(kfifo_out_flat);
+
+unsigned long kfifo_in_flat(struct kfifo *kfifo, const void *buff, unsigned long len)
+{
+    unsigned long unused;
+
+    unused = kfifo_unused(kfifo);
+    min_adj(len, unused);
+    kfifo_in_copy(kfifo, buff, len, kfifo->in);
+    kfifo->in += len;
+
+    return len;
+}
+EXPORT_SYMBOL(kfifo_in_flat);
+
+unsigned long kfifo_peek_record(struct kfifo *kfifo, void *buff,
+                                unsigned long len, unsigned long record)
+{
+    unsigned long datalen;
+
+    if (kfifo_empty(kfifo))
+        return 0;
+
+    datalen = kfifo_out_peek(kfifo, record);
+    min_adj(len, datalen);
+    kfifo_out_copy(kfifo, buff, len, kfifo->out + record);
+
+    return len;
+}
+EXPORT_SYMBOL(kfifo_peek_record);
+
+unsigned long kfifo_out_record(struct kfifo *kfifo, void *buff,
+                               unsigned long len, unsigned long record)
+{
+    unsigned long datalen;
+
+    if (kfifo_empty(kfifo))
+        return 0;
+
+    datalen = kfifo_out_peek(kfifo, record);
+    min_adj(len, datalen);
+    kfifo_out_copy(kfifo, buff, len, kfifo->out + record);
+	kfifo->out += datalen + record;
+
+    return len;
+}
+EXPORT_SYMBOL(kfifo_out_record);
+
+unsigned long kfifo_in_record(struct kfifo *kfifo, const void *buff,
+                              unsigned long len, unsigned long record)
+{
+    if (len + record > kfifo_unused(kfifo))
+        return 0;
+
+    kfifo_in_poke(kfifo, len, record);
+    kfifo_in_copy(kfifo, buff, len, kfifo->in + record);
+    kfifo->in += len + record;
+
+    return len;
+}
+EXPORT_SYMBOL(kfifo_in_record);
+
+state kfifo_dynamic_alloc(struct kfifo *kfifo, size_t esize, size_t size, gfp_t flags)
+{
+    size = roundup_power_of_2(size);
+    if (size < 2)
+        return -EINVAL;
+
+    kfifo->in = 0;
+    kfifo->out = 0;
+    kfifo->mask = size - 1;
+    kfifo->esize = esize;
+
+    kfifo->data = kmalloc_array(size, esize, flags);
+    if (!kfifo->data) {
+        kfifo->mask = 0;
+        return -EINVAL;
+    }
+
+    return -ENOERR;
+}
+EXPORT_SYMBOL(kfifo_dynamic_alloc);
+
+void kfifo_dynamic_free(struct kfifo *kfifo)
+{
+    kfree(kfifo->data);
+    kfifo->in = 0;
+    kfifo->out = 0;
+    kfifo->mask = 0;
+    kfifo->esize = 0;
+    kfifo->data = NULL;
+}
+EXPORT_SYMBOL(kfifo_dynamic_free);
