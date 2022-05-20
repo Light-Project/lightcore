4116dd18d0c0d83dc0341b1ac0f68ebff93b3d40
John-sanpe <sanpeqf@gmail.com>
2022-05-20 12:08:31 +0800

    Function: Added delay command for busy-waiting delay.

--- usr/command/sleep.c
+++ usr/command/sleep.c
@@ -7,26 +7,35 @@
 #include <initcall.h>
 #include <delay.h>
 
+#ifndef PROGRAM_NAME
+# define PROGRAM_NAME sleep
+# define PROGRAM_FUNC msleep
+# define PROGRAM_DESC "sleep millisecond"
+#else
+# define PROGRAM_FUNC mdelay
+# define PROGRAM_DESC "delay millisecond"
+#endif
+
 static void usage(struct kshell_context *ctx)
 {
-	kshell_printf(ctx, "usage: sleep [millisecond]\n");
+	kshell_printf(ctx, "usage: " __stringify(PROGRAM_NAME) " [millisecond]\n");
 }
 
-static state delay_main(struct kshell_context *ctx, int argc, char *argv[])
+static state sleep_main(struct kshell_context *ctx, int argc, char *argv[])
 {
-    if (argc < 2) {
+    if (argc != 2) {
         usage(ctx);
         return -EINVAL;
     }
 
-    msleep(atoi(argv[1]));
+    PROGRAM_FUNC(atoi(argv[1]));
     return -ENOERR;
 }
 
 static struct kshell_command sleep_cmd = {
-    .name = "sleep",
-    .desc = "sleep millisecond",
-    .exec = delay_main,
+    .name = __stringify(PROGRAM_NAME),
+    .desc = PROGRAM_DESC,
+    .exec = sleep_main,
 };
 
 static state sleep_init(void)
