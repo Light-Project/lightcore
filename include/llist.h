d2b5d7a303e57f907eb06a8202377b17fea88173
John-sanpe <sanpeqf@gmail.com>
2022-05-27 04:11:50 +0800

    Function: Added kernel lockless single list.
    [lock-list] Functional addition.
        -- This function is realized by using the CAS
            characteristics of the processor.
    
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ include/llist.h
@@ -0,0 +1,21 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _LLIST_H_
+#define _LLIST_H_
+
+#include <slist.h>
+#include <atomic.h>
+
+extern bool llist_split(struct slist_head *head, struct slist_head *new, struct slist_head *end);
+extern struct slist_head *llist_del(struct slist_head *head);
+
+static inline bool llist_add(struct slist_head *head, struct slist_head *new)
+{
+    return llist_split(head, new, new);
+}
+
+static inline struct slist_head *llist_destroy(struct slist_head *head)
+{
+    return xchg(&head->next, NULL);
+}
+
+#endif  /* _LLIST_H_ */
---
+++ lib/llist.c
@@ -0,0 +1,33 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <llist.h>
+#include <export.h>
+
+bool llist_split(struct slist_head *head, struct slist_head *new, struct slist_head *end)
+{
+	struct slist_head *first;
+
+    do first = end->next = READ_ONCE(head->next);
+    while (cmpxchg(&head->next, first, new) != first);
+
+    return !first;
+}
+EXPORT_SYMBOL(llist_split);
+
+struct slist_head *llist_del(struct slist_head *head)
+{
+	struct slist_head *old, *next, *entry;
+
+    for (entry = READ_ONCE(head->next); (old = entry);) {
+        next = READ_ONCE(entry->next);
+		entry = cmpxchg(&head->next, old, next);
+        if (old == entry)
+            break;
+    }
+
+    return entry;
+}
+EXPORT_SYMBOL(llist_del);
