b5e14b62083b2e9a3e8ee98fe0e1d99b894bc8b3
John-sanpe <sanpeqf@gmail.com>
2022-05-28 05:36:27 +0800

    Function: Lrange for interval mapping added.
    [lrange] Functional initialization.
        -- this framework is used to map the relationship between
            selectors and linear values, and can be used to convert
            device registers to actual values.
    
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ include/lrange.h
@@ -0,0 +1,51 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _LRANGE_H_
+#define _LRANGE_H_
+
+#include <types.h>
+#include <state.h>
+
+/**
+ * struct lrange - linear interval mapping.
+ * @base: base value in linear range.
+ * @min: lowest selector for linear range.
+ * @max: highest selector for linear range.
+ * @step: value step size.
+ */
+struct lrange {
+    unsigned int base;
+    unsigned int min, max;
+    unsigned int step;
+};
+
+#define LRANGE_STATIC(BASE, MIN, MAX, STEP) \
+    {.base = BASE, .min = MIN, .max = MAX, .step = STEP}
+
+#define LRANGE_INIT(BASE, MIN, MAX, STEP) \
+    (struct lrange) LRANGE_STATIC(BASE, MIN, MAX, STEP)
+
+#define DEFINE_LRANGE(NAME, BASE, MIN, MAX, STEP) \
+    struct lrange NAME = LRANGE_INIT(BASE, MIN, MAX, STEP)
+
+extern unsigned int lrange_value_limit(const struct lrange *lrange);
+extern unsigned int lrange_selector_range(const struct lrange *lrange);
+
+extern state lrange_get_value_low(const struct lrange *lrange, unsigned int selector, unsigned int *value);
+extern state lrange_get_value_high(const struct lrange *lrange, unsigned int selector, unsigned int *value);
+extern state lrange_get_value_low_within(const struct lrange *lrange, unsigned int selector, unsigned int *value);
+extern state lrange_get_value_high_within(const struct lrange *lrange, unsigned int selector, unsigned int *value);
+extern state lrange_get_selector_low(const struct lrange *lrange, unsigned int value, unsigned int *selector);
+extern state lrange_get_selector_high(const struct lrange *lrange, unsigned int value, unsigned int *selector);
+extern state lrange_get_selector_low_within(const struct lrange *lrange, unsigned int value, unsigned int *selector);
+extern state lrange_get_selector_high_within(const struct lrange *lrange, unsigned int value, unsigned int *selector);
+
+extern state lrange_array_get_value_low(const struct lrange *lrange, unsigned int num, unsigned int selector, unsigned int *value);
+extern state lrange_array_get_value_high(const struct lrange *lrange, unsigned int num, unsigned int selector, unsigned int *value);
+extern state lrange_array_get_value_low_within(const struct lrange *lrange, unsigned int num, unsigned int selector, unsigned int *value);
+extern state lrange_array_get_value_high_within(const struct lrange *lrange, unsigned int num, unsigned int selector, unsigned int *value);
+extern state lrange_array_get_selector_low(const struct lrange *lrange, unsigned int num, unsigned int value, unsigned int *selector);
+extern state lrange_array_get_selector_high(const struct lrange *lrange, unsigned int num, unsigned int value, unsigned int *selector);
+extern state lrange_array_get_selector_low_within(const struct lrange *lrange, unsigned int num, unsigned int value, unsigned int *selector);
+extern state lrange_array_get_selector_high_within(const struct lrange *lrange, unsigned int num, unsigned int value, unsigned int *selector);
+
+#endif  /* _LRANGE_H_ */
---
+++ lib/lrange.c
@@ -0,0 +1,128 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <lrange.h>
+#include <math.h>
+#include <export.h>
+
+unsigned int lrange_value_limit(const struct lrange *lrange)
+{
+    return lrange->base + (lrange->max - lrange->min) * lrange->step;
+}
+EXPORT_SYMBOL(lrange_value_limit);
+
+unsigned int lrange_selector_range(const struct lrange *lrange)
+{
+    return lrange->max - lrange->min + 1;
+}
+EXPORT_SYMBOL(lrange_selector_range);
+
+#define GENERIC_LRANGE_GET_VALUE_OP(name, expression)                                           \
+state lrange_get_value_##name                                                                   \
+(const struct lrange *lrange, unsigned int selector, unsigned int *value)                       \
+{                                                                                               \
+    if (selector < lrange->min || selector > lrange->max)                                       \
+        return -EINVAL;                                                                         \
+                                                                                                \
+    *value = expression;                                                                        \
+    return -ENOERR;                                                                             \
+}                                                                                               \
+EXPORT_SYMBOL(lrange_get_value_##name)
+
+GENERIC_LRANGE_GET_VALUE_OP(low, lrange->base + (selector - lrange->min) * lrange->step);
+GENERIC_LRANGE_GET_VALUE_OP(high, lrange->base + (selector - lrange->min) * lrange->step + (lrange->step - 1));
+
+#define GENERIC_LRANGE_GET_VALUE_WITHIN_OP(name, expression)                                    \
+state lrange_get_value_##name##_within                                                          \
+(const struct lrange *lrange, unsigned int selector, unsigned int *value)                       \
+{                                                                                               \
+    if (selector < lrange->min) {                                                               \
+        *value = lrange->base;                                                                  \
+        return -EINVAL;                                                                         \
+    }                                                                                           \
+                                                                                                \
+    if (selector > lrange->max) {                                                               \
+        *value = lrange_value_limit(lrange);                                                    \
+        return -EINVAL;                                                                         \
+    }                                                                                           \
+                                                                                                \
+    *value = expression;                                                                        \
+    return -ENOERR;                                                                             \
+}                                                                                               \
+EXPORT_SYMBOL(lrange_get_value_##name##_within)
+
+GENERIC_LRANGE_GET_VALUE_WITHIN_OP(low, lrange->base + (selector - lrange->min) * lrange->step);
+GENERIC_LRANGE_GET_VALUE_WITHIN_OP(high, lrange->base + (selector - lrange->min) * lrange->step + (lrange->step - 1));
+
+#define GENERIC_LRANGE_GET_SELECTOR_OP(name, hstep, nstep)                                      \
+state lrange_get_selector_##name                                                                \
+(const struct lrange *lrange, unsigned int value, unsigned int *selector)                       \
+{                                                                                               \
+    if (value < lrange->base || value > lrange_value_limit(lrange))                             \
+        return -EINVAL;                                                                         \
+                                                                                                \
+    if (lrange->step)                                                                           \
+        *selector = hstep;                                                                      \
+    else                                                                                        \
+        *selector = nstep;                                                                      \
+                                                                                                \
+    return -ENOERR;                                                                             \
+}                                                                                               \
+EXPORT_SYMBOL(lrange_get_selector_##name)
+
+GENERIC_LRANGE_GET_SELECTOR_OP(low, lrange->min + (value - lrange->base) / lrange->step, lrange->min);
+GENERIC_LRANGE_GET_SELECTOR_OP(high, lrange->min + DIV_ROUND_UP(value - lrange->base, lrange->step), lrange->max);
+
+#define GENERIC_LRANGE_GET_SELECTOR_WITHIN_OP(name, hstep, nstep)                               \
+state lrange_get_selector_##name##_within                                                       \
+(const struct lrange *lrange, unsigned int value, unsigned int *selector)                       \
+{                                                                                               \
+    if (value < lrange->base) {                                                                 \
+        *selector = lrange->min;                                                                \
+        return -EINVAL;                                                                         \
+    }                                                                                           \
+                                                                                                \
+    if (value > lrange_value_limit(lrange)) {                                                   \
+        *selector = lrange->max;                                                                \
+        return -EINVAL;                                                                         \
+    }                                                                                           \
+                                                                                                \
+    if (lrange->step)                                                                           \
+        *selector = hstep;                                                                      \
+    else                                                                                        \
+        *selector = nstep;                                                                      \
+                                                                                                \
+    return -ENOERR;                                                                             \
+}                                                                                               \
+EXPORT_SYMBOL(lrange_get_selector_##name##_within)
+
+GENERIC_LRANGE_GET_SELECTOR_WITHIN_OP(low, lrange->min + (value - lrange->base) / lrange->step, lrange->min);
+GENERIC_LRANGE_GET_SELECTOR_WITHIN_OP(high, lrange->min + DIV_ROUND_UP(value - lrange->base, lrange->step), lrange->max);
+
+#define GENERIC_LRANGE_ARRAY_GET_OP(name)                                                       \
+state lrange_array_get##name                                                                    \
+(const struct lrange *lrange, unsigned int num, unsigned int in, unsigned int *out)             \
+{                                                                                               \
+    unsigned int count;                                                                         \
+    state retval = -EINVAL;                                                                     \
+                                                                                                \
+    for (count = 0; count < num; ++count) {                                                     \
+        retval = lrange_get_##name(lrange + num, in, out);                                      \
+        if (!retval)                                                                            \
+            break;                                                                              \
+    }                                                                                           \
+                                                                                                \
+    return retval;                                                                              \
+}                                                                                               \
+EXPORT_SYMBOL(lrange_array_get##name)
+
+GENERIC_LRANGE_ARRAY_GET_OP(value_low);
+GENERIC_LRANGE_ARRAY_GET_OP(value_high);
+GENERIC_LRANGE_ARRAY_GET_OP(value_low_within);
+GENERIC_LRANGE_ARRAY_GET_OP(value_high_within);
+GENERIC_LRANGE_ARRAY_GET_OP(selector_low);
+GENERIC_LRANGE_ARRAY_GET_OP(selector_high);
+GENERIC_LRANGE_ARRAY_GET_OP(selector_low_within);
+GENERIC_LRANGE_ARRAY_GET_OP(selector_high_within);
