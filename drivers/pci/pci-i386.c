3a9c3b7ac36ea38386b261dd6609dcb78da1a241
John-sanpe <sanpeqf@gmail.com>
2022-06-01 15:14:35 +0800

    Maintain: Fixed the problem of i386pci driver under multi-threading.

--- drivers/pci/pci-i386.c
+++ drivers/pci/pci-i386.c
@@ -6,25 +6,41 @@
 #define DRIVER_NAME "i386-pci"
 
 #include <initcall.h>
+#include <ioops.h>
 #include <driver/platform.h>
 #include <driver/pci.h>
-#include <asm/io.h>
+
+#define I386_PCI_CONFIG(bus, devfn, reg) ( \
+    BIT(31) | (bus << 16) | (devfn << 8) | \
+    ((reg & 0xF00) << 16) | (reg & 0xfc) \
+)
+
+static SPIN_LOCK(i386_pci_lock);
 
 state pci_raw_config_read(unsigned int domain, unsigned int bus, unsigned int devfn,
                           unsigned int reg, int size, uint32_t *val)
 {
-    uint32_t addr = (1 << 31) | (bus << 16) | (PCI_SLOT(devfn) << 11) |
-                    (PCI_FUNC(devfn) << 8) | reg;
+    unsigned int offset = reg & 0x03;
+    irqflags_t irqflags;
+
+    if (domain || (bus > 255) || (devfn > 255) || (reg > 4095)) {
+        *val = UINT32_MAX;
+        return -EINVAL;
+    }
+
+    spin_lock_irqsave(&i386_pci_lock, &irqflags);
 
     /* Use North Bridge Port */
-    outl(0x0cf8, addr);
+    outl(0x0cf8, I386_PCI_CONFIG(bus, devfn, reg));
 
     if (size == 1)
-        *val = inb(0x0cfc);
+        *val = inb(0x0cfc + offset);
     else if (size == 2)
-        *val = inw(0x0cfc);
+        *val = inw(0x0cfc + offset);
     else
-        *val = inl(0x0cfc);
+        *val = inl(0x0cfc + offset);
+
+    spin_unlock_irqrestore(&i386_pci_lock, &irqflags);
 
     return -ENOERR;
 }
@@ -32,18 +48,25 @@ state pci_raw_config_read(unsigned int domain, unsigned int bus, unsigned int de
 state pci_raw_config_write(unsigned int domain, unsigned int bus, unsigned int devfn,
                            unsigned int reg, int size, uint32_t val)
 {
-    uint32_t addr = (1 << 31) | (bus << 16) | (PCI_SLOT(devfn) << 11) |
-                    (PCI_FUNC(devfn)<<8) | reg;
+    unsigned int offset = reg & 0x03;
+    irqflags_t irqflags;
+
+    if (domain || (bus > 255) || (devfn > 255) || (reg > 4095))
+        return -EINVAL;
 
     /* Use North Bridge Port */
-    outl(0x0cf8, addr);
+    outl(0x0cf8, I386_PCI_CONFIG(bus, devfn, reg));
+
+    spin_lock_irqsave(&i386_pci_lock, &irqflags);
 
     if (size == 1)
-        outb(0x0cfc, val);
+        outb(0x0cfc, val + offset);
     else if (size == 2)
-        outw(0x0cfc, val);
+        outw(0x0cfc, val + offset);
     else
-        outl(0x0cfc, val);
+        outl(0x0cfc, val + offset);
+
+    spin_unlock_irqrestore(&i386_pci_lock, &irqflags);
 
     return -ENOERR;
 }
