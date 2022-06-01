536e4c507e10967904a7eaa6a7475aa936481fae
John-sanpe <sanpeqf@gmail.com>
2022-06-02 07:27:01 +0800

    Function: Added errname for showing state name.

---
+++ include/errname.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _ERRNAME_H_
+#define _ERRNAME_H_
+
+#include <types.h>
+#include <state.h>
+
+extern const char *errname(state error);
+
+#endif  /* _ERRNAME_H_ */
