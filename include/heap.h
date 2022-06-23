48ad4ee54e8b67debb72597c43ea6db4caf1f1b6
John-sanpe <sanpeqf@gmail.com>
2022-06-24 01:54:59 +0800

    Function: Added heap library first time.

---
+++ include/heap.h
@@ -0,0 +1,203 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _HEAP_H_
+#define _HEAP_H_
+
+#include <kernel.h>
+#include <poison.h>
+
+struct heap_node {
+    struct heap_node *parent;
+    struct heap_node *left;
+    struct heap_node *right;
+};
+
+struct heap_root {
+    struct heap_node *node;
+    unsigned int count;
+};
+
+#define HEAP_STATIC \
+    {NULL, 0}
+
+#define HEAP_CACHED_STATIC \
+    {{NULL}, NULL}
+
+#define HEAP_INIT \
+    (struct heap_root) HEAP_STATIC
+
+#define HEAP_ROOT(name) \
+    struct heap_root name = HEAP_INIT
+
+/**
+ * heap_entry - get the struct for this entry.
+ * @ptr: the &struct heap_node pointer.
+ * @type: the type of the struct this is embedded in.
+ * @member: the name of the heap_node within the struct.
+ */
+#define	heap_entry(ptr, type, member) \
+    container_of(ptr, type, member)
+
+/**
+ * heap_entry_safe - get the struct for this entry or null.
+ * @ptr: the &struct heap_node pointer.
+ * @type: the type of the struct this is embedded in.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_entry_safe(ptr, type, member) \
+    container_of_safe(ptr, type, member)
+
+#if defined(__KERNEL__) && defined(CONFIG_DEBUG_HEAP)
+extern bool heap_debug_link_check(struct heap_node *parent, struct heap_node **link, struct heap_node *node);
+extern bool heap_debug_delete_check(struct heap_node *node);
+#define DEBUG_HEAP
+#endif
+
+typedef long (*heap_cmp_t)(const struct heap_node *nodea, const struct heap_node *nodeb);
+extern void heap_fixup(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp);
+extern void heap_erase(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp);
+extern struct heap_node *heap_remove(struct heap_root *root, struct heap_node *node);
+extern struct heap_node **heap_parent(struct heap_root *root, struct heap_node **parentp, struct heap_node *node);
+extern struct heap_node *heap_find(struct heap_root *root, unsigned int index);
+
+/* Preorder iteration (Root-first) - always access the left node first */
+extern struct heap_node *heap_level_first(const struct heap_root *root, unsigned long *index);
+extern struct heap_node *heap_level_next(const struct heap_root *root, unsigned long *index);
+
+/**
+ * heap_pre_first_entry - get the preorder first element from a heaptree.
+ * @ptr: the heaptree root to take the element from.
+ * @type: the type of the struct this is embedded in.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_first_entry(root, index, type, member) \
+    heap_entry_safe(heap_level_first(root, index), type, member)
+
+/**
+ * heap_pre_next_entry - get the preorder next element in heaptree.
+ * @pos: the type * to cursor.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_next_entry(root, index, type, member) \
+    heap_entry_safe(heap_level_next(root, index), type, member)
+
+/**
+ * heap_pre_for_each - preorder iterate over a heaptree.
+ * @pos: the &struct heap_node to use as a loop cursor.
+ * @root: the root for your heaptree.
+ */
+#define heap_for_each(pos, index, root) \
+    for (pos = heap_level_first(root, index); \
+         pos; pos = heap_level_next(root, index))
+
+/**
+ * heap_pre_for_each_from - preorder iterate over a heaptree from the current point.
+ * @pos: the &struct heap_node to use as a loop cursor.
+ */
+#define heap_for_each_from(pos, index, root) \
+    for (; pos; pos = heap_level_next(root, index))
+
+/**
+ * heap_pre_for_each_continue - continue preorder iteration over a heaptree.
+ * @pos: the &struct heap_node to use as a loop cursor.
+ */
+#define heap_for_each_continue(pos, index, root) \
+    for (pos = heap_level_next(root, index); \
+         pos; pos = heap_level_next(root, index))
+
+/**
+ * heap_pre_for_each_entry - preorder iterate over heaptree of given type.
+ * @pos: the type * to use as a loop cursor.
+ * @root: the root for your heaptree.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_for_each_entry(pos, index, root, member) \
+    for (pos = heap_first_entry(root, index, typeof(*pos), member); \
+         pos; pos = heap_next_entry(root, index, typeof(*pos), member))
+
+/**
+ * heap_pre_for_each_entry_from - preorder iterate over heaptree of given type from the current point.
+ * @pos: the type * to use as a loop cursor.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_for_each_entry_from(pos, index, root, member) \
+    for (; pos; pos = heap_next_entry(root, index, typeof(*pos), member))
+
+/**
+ * heap_pre_for_each_entry_continue - continue preorder iteration over heaptree of given type.
+ * @pos: the type * to use as a loop cursor.
+ * @member: the name of the heap_node within the struct.
+ */
+#define heap_for_each_entry_continue(pos, index, root, member) \
+    for (pos = heap_next_entry(root, index, typeof(*pos), member); \
+         pos; pos = heap_next_entry(root, index, typeof(*pos), member))
+
+/**
+ * heap_link - link node to parent.
+ * @parent: point to parent node.
+ * @link: point to pointer to child node.
+ * @node: new node to link.
+ */
+static inline void heap_link(struct heap_node *parent, struct heap_node **link, struct heap_node *node)
+{
+#ifdef DEBUG_HEAP
+    if (unlikely(!heap_debug_link_check(parent, link, node)))
+        return;
+#endif
+
+    /* link = &parent->left/right */
+    *link = node;
+    node->parent = parent;
+    node->left = node->right = NULL;
+}
+
+/**
+ * heap_insert_node - link node to parent and fixup heaptree.
+ * @root: heaptree root of node.
+ * @parent: parent node of node.
+ * @link: point to pointer to child node.
+ * @node: new node to link.
+ */
+static inline void heap_insert_node(struct heap_root *root, struct heap_node *parent, struct heap_node **link,
+                                    struct heap_node *node, heap_cmp_t cmp)
+{
+    heap_link(parent, link, node);
+    heap_fixup(root, node, cmp);
+}
+
+/**
+ * heap_insert - find the parent node and insert new node.
+ * @root: heaptree root of node.
+ * @node: new node to insert.
+ * @cmp: operator defining the node order.
+ */
+static inline void heap_insert(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp)
+{
+    struct heap_node *parent, **link;
+
+    link = heap_parent(root, &parent, node);
+    heap_insert_node(root, parent, link, node, cmp);
+}
+
+/**
+ * heap_delete - delete node and fixup heaptree.
+ * @root: heaptree root of node.
+ * @node: node to delete.
+ */
+static inline void heap_delete(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp)
+{
+    struct heap_node *rebalance;
+
+#ifdef DEBUG_HEAP
+    if (unlikely(!heap_debug_delete_check(node)))
+        return;
+#endif
+
+    if ((rebalance = heap_remove(root, node)))
+        heap_erase(root, rebalance, cmp);
+
+    node->left = POISON_HPNODE1;
+    node->right = POISON_HPNODE2;
+    node->parent = POISON_HPNODE3;
+}
+
+#endif  /* _HEAP_H_ */
---
+++ lib/heap.c
@@ -0,0 +1,235 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <heap.h>
+#include <titer.h>
+#include <export.h>
+
+static __always_inline void
+parent_swap(struct heap_root *root, struct heap_node *parent, struct heap_node *node)
+{
+    struct heap_node *gparent = parent->parent;
+    struct heap_node shadow = *node;
+
+    if (node->left)
+        node->left->parent = parent;
+    if (node->right)
+        node->right->parent = parent;
+
+    if (parent->left == node) {
+        node->left = parent;
+        if ((node->right = parent->right))
+            parent->right->parent = node;
+    } else { /* parent->right == node */
+        node->right = parent;
+        if ((node->left = parent->left))
+            parent->left->parent = node;
+    }
+
+    if (!(node->parent = gparent))
+        root->node = node;
+    else if (gparent->left == parent)
+        gparent->left = node;
+    else /* gparent->right == parent */
+        gparent->right = node;
+
+    *parent = shadow;
+    parent->parent = node;
+}
+
+/**
+ * heap_fixup - balance after insert node.
+ * @root: heap root of node.
+ * @node: new inserted node.
+ * @cmp: operator defining the node order.
+ */
+void heap_fixup(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp)
+{
+    struct heap_node *parent;
+
+    while ((parent = node->parent)) {
+        if (cmp(node, parent) >= 0)
+            break;
+        parent_swap(root, parent, node);
+    }
+
+    root->count++;
+}
+EXPORT_SYMBOL(heap_fixup);
+
+/**
+ * heap_erase - balance after remove node.
+ * @root: heap root of node.
+ * @node: removed node.
+ * @cmp: operator defining the node order.
+ */
+void heap_erase(struct heap_root *root, struct heap_node *node, heap_cmp_t cmp)
+{
+    struct heap_node *successor = node->parent;
+    struct heap_node *child1, *child2;
+    long result;
+
+    if (successor && (result = cmp(node, successor)) < 0)
+        heap_fixup(root, node, cmp);
+
+    else for (;;) {
+        child1 = node->left;
+        child2 = node->right;
+
+        if (!child1 && !child2)
+            return;
+        else if (!child2)
+            successor = node->left;
+        else if (!child1)
+            successor = node->left;
+        else { /* child1 && child2 */
+            if (cmp(node->left, node->right) < 0)
+                successor = node->left;
+            else
+                successor = node->right;
+        }
+
+        if (cmp(node, successor) < 0)
+            return;
+        parent_swap(root, node, successor);
+    }
+}
+EXPORT_SYMBOL(heap_erase);
+
+/**
+ * heap_remove - remove node form heap.
+ * @root: heap root of node.
+ * @node: node to remove.
+ */
+struct heap_node *heap_remove(struct heap_root *root, struct heap_node *node)
+{
+    struct heap_node *successor = heap_find(root, root->count);
+
+    if (!successor->parent) {
+        /*
+         * Case 1: node to delete is root.
+         *
+         *   (r)      (p)
+         *    |        |
+         *   (n)  ->  null
+         *
+         */
+
+        root->node = NULL;
+        successor = NULL;
+    } else {
+        struct heap_node *parent;
+
+        parent = successor->parent;
+        if (parent->left == successor)
+            parent->left = NULL;
+        else /* parent->right == successor */
+            parent->right = NULL;
+
+        /*
+         * Case 2: node to delete is successor.
+         *
+         *     (p)          (p)
+         *     / \          /
+         *   (s) (n)  ->  (s)
+         *
+         */
+
+        if (node == successor) {
+            successor = NULL;
+            goto finish;
+        }
+
+        /*
+         * Case 3: node to delete is normal.
+         *
+         *         (p)              (p)
+         *        /   \             / \
+         *       /     \          (x) (s)
+         *     (n)     (s)  ->    / \
+         *     / \     /        (z) (y)
+         *   (x) (y) (z)
+         *
+         */
+
+        /* inheritance information */
+        *successor = *node;
+
+        /* inheritance relationship */
+        if (node->left)
+            node->left->parent = successor;
+        if (node->right)
+            node->right->parent = successor;
+
+        if (!(parent = node->parent))
+            root->node = successor;
+        else {
+            if (node == parent->left)
+                parent->left = successor;
+            else if (node == parent->right)
+                parent->right = successor;
+        }
+    }
+
+finish:
+    root->count--;
+    return successor;
+}
+EXPORT_SYMBOL(heap_remove);
+
+/**
+ * heap_parent - find the parent node.
+ * @root: heap tree want to search.
+ * @parentp: pointer used to modify the parent node pointer.
+ * @node: new node to insert.
+ */
+struct heap_node **
+heap_parent(struct heap_root *root, struct heap_node **parentp, struct heap_node *node)
+{
+    unsigned int depth = flsuf(root->count + 1);
+    struct heap_node **link;
+
+    link = &root->node;
+    if (unlikely(!*link)) {
+        *parentp = NULL;
+        return link;
+    }
+
+    while (depth--) {
+        *parentp = *link;
+        if ((root->count + 1) & BIT(depth))
+            link = &(*link)->right;
+        else
+            link = &(*link)->left;
+    };
+
+    return link;
+}
+EXPORT_SYMBOL(heap_parent);
+
+/**
+ * heap_find - find @index in tree @root.
+ * @root: heap tree want to search.
+ * @index: index of node.
+ */
+struct heap_node *heap_find(struct heap_root *root, unsigned int index)
+{
+    unsigned int depth = flsuf(root->count);
+    struct heap_node *node = root->node;
+
+    while (node && depth--) {
+        if ((root->count) & BIT(depth))
+            node = node->right;
+        else
+            node = node->left;
+    };
+
+    return node;
+}
+EXPORT_SYMBOL(heap_find);
+
+TITER_LEVELORDER_DEFINE(, heap, struct heap_root, node, struct heap_node, left, right)
+EXPORT_SYMBOL(heap_level_first);
+EXPORT_SYMBOL(heap_level_next);
