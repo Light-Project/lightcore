8e62ab120d8eaaded868b3facb172d311547b143
John-sanpe <sanpeqf@gmail.com>
2022-06-26 17:05:17 +0800

    Supplement: Added poison for heap node.

--- include/poison.h
+++ include/poison.h
@@ -20,4 +20,8 @@
 #define POISON_RBNODE2  ((void *) POISON_OFFSET + 0x410)
 #define POISON_RBNODE3  ((void *) POISON_OFFSET + 0x420)
 
+#define POISON_HPNODE1  ((void *) POISON_OFFSET + 0x500)
+#define POISON_HPNODE2  ((void *) POISON_OFFSET + 0x510)
+#define POISON_HPNODE3  ((void *) POISON_OFFSET + 0x520)
+
 #endif  /* _POISON_H_ */
