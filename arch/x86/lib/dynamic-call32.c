7f2078077b9c0302cc9afdc81edc0131940e96ca
John-sanpe <sanpeqf@gmail.com>
2022-07-03 13:16:20 +0800

    Function: Added dynamic-call library for x86.

---
+++ arch/x86/lib/dynamic-call32.c
@@ -0,0 +1,29 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <dynamic-call.h>
+#include <export.h>
+
+long dynamic_call(dynamic_call_t fn, unsigned int num, void *args[])
+{
+    long retval;
+
+    asm volatile (
+        "pushl  %%ebp           \n"
+        "movl   %%esp, %%ebp    \n"
+        "1:                     \n"
+        "subl   $4, %%eax       \n"
+        "pushl  (%%eax)         \n"
+        "loop   1b              \n"
+        "call   *%%ebx          \n"
+        "leave                  \n"
+        : "=a"(retval)
+        : "0"(args + num), "c"(num), "b"(fn)
+        : "memory"
+    );
+
+    return retval;
+}
+EXPORT_SYMBOL(dynamic_call);
---
+++ include/dynamic-call.h
@@ -0,0 +1,10 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _DYNAMIC_CALL_H_
+#define _DYNAMIC_CALL_H_
+
+#include <types.h>
+
+typedef long (*dynamic_call_t)(void);
+extern long dynamic_call(dynamic_call_t fn, unsigned int num, void *args[]);
+
+#endif  /* _DYNAMIC_CALL_H_ */
