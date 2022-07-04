1c7663e2b7848344791028daeb6472e1aa889d68
John-sanpe <sanpeqf@gmail.com>
2022-07-04 11:50:20 +0800

    Function: Added sha1 algorithm for crypto library.

--- include/crypto.h
+++ include/crypto.h
@@ -19,6 +19,20 @@ static inline void chacha20_block(uint32_t *state, uint8_t *stream)
     chacha_block(state, stream, 20);
 }
 
+#define SHA1_DIGEST_SIZE        20
+#define SHA1_BLOCK_SIZE         64
+#define SHA1_DIGEST_WORDS       (SHA1_DIGEST_SIZE / BYTES_PER_U32)
+#define SHA1_WORKSPACE_WORDS    (SHA1_BLOCK_SIZE / BYTES_PER_U32)
+
+#define SHA1_DIGEST0 0x67452301UL
+#define SHA1_DIGEST1 0xefcdab89UL
+#define SHA1_DIGEST2 0x98badcfeUL
+#define SHA1_DIGEST3 0x10325476UL
+#define SHA1_DIGEST4 0xc3d2e1f0UL
+
+extern void sha1_transform(uint32_t digest[SHA1_DIGEST_WORDS], uint32_t buff[SHA1_WORKSPACE_WORDS], const char *data);
+extern void sha1_init(uint32_t digest[SHA1_DIGEST_WORDS]);
+
 extern uint8_t crc4(uint8_t *src, int len, uint8_t crc);
 extern uint8_t crc8(uint8_t *src, int len, uint8_t crc);
 extern uint16_t crc16(uint8_t *src, int len, uint16_t crc);
--- lib/crypto/sha1.c
+++ lib/crypto/sha1.c
@@ -1,127 +1,80 @@
-#include <types.h>
-#include <string.h>
-#include <sha1.h>
-
-#define rol(bits, value)	(((value) << (bits)) | ((value) >> (32 - (bits))))
-
-static void sha1_transform(struct sha1_ctx_t * ctx)
-{
-	uint32_t W[80];
-	uint32_t A, B, C, D, E;
-	uint8_t * p = ctx->buf;
-	int t;
-
-	for(t = 0; t < 16; ++t)
-	{
-		uint32_t tmp =  *p++ << 24;
-		tmp |= *p++ << 16;
-		tmp |= *p++ << 8;
-		tmp |= *p++;
-		W[t] = tmp;
-	}
-
-	for(; t < 80; t++)
-	{
-		W[t] = rol(1, W[t-3] ^ W[t-8] ^ W[t-14] ^ W[t-16]);
-	}
-
-	A = ctx->state[0];
-	B = ctx->state[1];
-	C = ctx->state[2];
-	D = ctx->state[3];
-	E = ctx->state[4];
-
-	for(t = 0; t < 80; t++)
-	{
-		uint32_t tmp = rol(5, A) + E + W[t];
-
-		if (t < 20)
-			tmp += (D ^ (B & (C ^ D))) + 0x5A827999;
-		else if ( t < 40)
-			tmp += (B ^ C ^ D) + 0x6ED9EBA1;
-		else if ( t < 60)
-			tmp += ((B & C) | (D & (B | C))) + 0x8F1BBCDC;
-		else
-			tmp += (B ^ C ^ D) + 0xCA62C1D6;
-
-		E = D;
-		D = C;
-		C = rol(30, B);
-		B = A;
-		A = tmp;
-	}
-
-	ctx->state[0] += A;
-	ctx->state[1] += B;
-	ctx->state[2] += C;
-	ctx->state[3] += D;
-	ctx->state[4] += E;
-}
-
-void sha1_init(struct sha1_ctx_t * ctx)
-{
-	ctx->state[0] = 0x67452301;
-	ctx->state[1] = 0xEFCDAB89;
-	ctx->state[2] = 0x98BADCFE;
-	ctx->state[3] = 0x10325476;
-	ctx->state[4] = 0xC3D2E1F0;
-	ctx->count = 0;
-}
-
-void sha1_update(struct sha1_ctx_t * ctx, const void * data, int len)
-{
-	int i = (int)(ctx->count & 63);
-	const uint8_t * p = (const uint8_t *)data;
-
-	ctx->count += len;
-	while(len--)
-	{
-		ctx->buf[i++] = *p++;
-		if (i == 64)
-		{
-			sha1_transform(ctx);
-			i = 0;
-		}
-	}
-}
-
-const uint8_t * sha1_final(struct sha1_ctx_t * ctx)
-{
-	uint8_t * p = ctx->buf;
-	uint64_t cnt = ctx->count * 8;
-	int i;
-
-	sha1_update(ctx, (uint8_t *)"\x80", 1);
-	while((ctx->count & 63) != 56)
-	{
-		sha1_update(ctx, (uint8_t *)"\0", 1);
-	}
-	for(i = 0; i < 8; ++i)
-	{
-		uint8_t tmp = (uint8_t)(cnt >> ((7 - i) * 8));
-		sha1_update(ctx, &tmp, 1);
-	}
-
-	for(i = 0; i < 5; i++)
-		{
-		uint32_t tmp = ctx->state[i];
-		*p++ = tmp >> 24;
-		*p++ = tmp >> 16;
-		*p++ = tmp >> 8;
-		*p++ = tmp >> 0;
-	}
-
-	return ctx->buf;
-}
-
+/* SPDX-License-Identifier: GPL-2.0-or-later */
 /*
- * Compute sha1 (160-bits) message digest
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
  */
-const uint8_t * sha1_hash(const void * data, int len, uint8_t * digest)
+
+#include <crypto.h>
+#include <bitops.h>
+#include <proc.h>
+#include <export.h>
+
+#define W(count, buff) (buff[(count) & 0x0f])
+#define SHA1_MIX(c, b, d) rol32(W(c + 13, b) ^ W(c + 8, b) ^ W(c +  2, b) ^ W(c, b), 1)
+#define SHA1_SRC(c, b, d) unaligned_get_be32((uint32_t *)d + c)
+
+#define SHA1_ROUND(count, buff, data, A, B, C, D, E, func, const, exp) do { \
+    uint32_t value = func(count, buff, data);                               \
+    W(count, buff) = value; E += value + rol32(A, 5) + (exp) + (const);     \
+    B = ror32(B, 2); value = E; E = D; D = C; C = B; B = A; A = value;      \
+} while (0)
+
+#define SHA1_00_15(c, b, d, A, B, C, D, E) \
+        SHA1_ROUND(c, b, d, A, B, C, D, E, SHA1_MIX, 0x5a827999, (((C ^ D) & B) ^ D))
+#define SHA1_16_19(c, b, d, A, B, C, D, E) \
+        SHA1_ROUND(c, b, d, A, B, C, D, E, SHA1_SRC, 0x5a827999, (((C ^ D) & B) ^ D))
+#define SHA1_20_39(c, b, d, A, B, C, D, E) \
+        SHA1_ROUND(c, b, d, A, B, C, D, E, SHA1_SRC, 0x6ed9eba1, (B ^ C ^ D))
+#define SHA1_40_59(c, b, d, A, B, C, D, E) \
+        SHA1_ROUND(c, b, d, A, B, C, D, E, SHA1_SRC, 0x8f1bbcdc, ((B & C) + (D & (B ^ C))))
+#define SHA1_60_79(c, b, d, A, B, C, D, E) \
+        SHA1_ROUND(c, b, d, A, B, C, D, E, SHA1_SRC, 0xca62c1d6, (B ^ C ^ D))
+
+void sha1_transform(uint32_t digest[SHA1_DIGEST_WORDS],
+                    uint32_t buff[SHA1_WORKSPACE_WORDS], const char *data)
 {
-	struct sha1_ctx_t ctx;
-	sha1_init(&ctx);
-	sha1_update(&ctx, data, len);
-	memcpy(digest, sha1_final(&ctx), SHA1_DIGEST_SIZE);
-	return digest;
+    uint32_t A, B, C, D, E;
+    unsigned int count;
+
+    A = digest[0];
+    B = digest[1];
+    C = digest[2];
+    D = digest[3];
+    E = digest[4];
+
+	/* Round 1 SRC */
+    for (count = 0; count < 16; ++count)
+        SHA1_00_15(count, buff, data, A, B, C, D, E);
+
+	/* Round 1 MIX */
+    for (count = 16; count < 20; ++count)
+        SHA1_16_19(count, buff, data, A, B, C, D, E);
+
+	/* Round 2 */
+    for (count = 20; count < 40; ++count)
+        SHA1_20_39(count, buff, data, A, B, C, D, E);
+
+	/* Round 3 */
+    for (count = 40; count < 60; ++count)
+        SHA1_40_59(count, buff, data, A, B, C, D, E);
+
+	/* Round 4 */
+    for (count = 60; count < 80; ++count)
+        SHA1_60_79(count, buff, data, A, B, C, D, E);
+
+    digest[0] += A;
+    digest[1] += B;
+    digest[2] += C;
+    digest[3] += D;
+    digest[4] += E;
 }
+EXPORT_SYMBOL(sha1_transform);
+
+void sha1_init(uint32_t digest[SHA1_DIGEST_WORDS])
+{
+    digest[0] = SHA1_DIGEST0;
+    digest[1] = SHA1_DIGEST1;
+    digest[2] = SHA1_DIGEST2;
+    digest[3] = SHA1_DIGEST3;
+    digest[4] = SHA1_DIGEST4;
+}
+EXPORT_SYMBOL(sha1_init);
