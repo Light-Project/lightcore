efef9e506ce919d25617a9e745326845bfd387dd
John-sanpe <sanpeqf@gmail.com>
2022-07-05 17:32:09 +0800

    Function: Added sha256 algorithm for crypto library.

---
+++ include/crypto/sha256.h
@@ -0,0 +1,30 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _CRYPTO_SHA256_H_
+#define _CRYPTO_SHA256_H_
+
+#include <types.h>
+
+#define SHA224_DIGEST0 0xc1059ed8UL
+#define SHA224_DIGEST1 0x367cd507UL
+#define SHA224_DIGEST2 0x3070dd17UL
+#define SHA224_DIGEST3 0xf70e5939UL
+#define SHA224_DIGEST4 0xffc00b31UL
+#define SHA224_DIGEST5 0x68581511UL
+#define SHA224_DIGEST6 0x64f98fa7UL
+#define SHA224_DIGEST7 0xbefa4fa4UL
+
+#define SHA256_DIGEST0 0x6a09e667UL
+#define SHA256_DIGEST1 0xbb67ae85UL
+#define SHA256_DIGEST2 0x3c6ef372UL
+#define SHA256_DIGEST3 0xa54ff53aUL
+#define SHA256_DIGEST4 0x510e527fUL
+#define SHA256_DIGEST5 0x9b05688cUL
+#define SHA256_DIGEST6 0x1f83d9abUL
+#define SHA256_DIGEST7 0x5be0cd19UL
+
+extern void sha256_transform(uint32_t *digest, uint32_t *buff, const char *data);
+extern void sha224_transform(uint32_t *digest, uint32_t *buff, const char *data);
+extern void sha256_init(uint32_t *digest);
+extern void sha224_init(uint32_t *digest);
+
+#endif  /* _CRYPTO_SHA256_H_ */
--- lib/crypto/sha256.c
+++ lib/crypto/sha256.c
@@ -1,158 +1,139 @@
-#include <types.h>
-#include <string.h>
-#include <sha256.h>
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
 
-#define ror(value, bits)	(((value) >> (bits)) | ((value) << (32 - (bits))))
-#define shr(value, bits)	((value) >> (bits))
+#include <crypto/sha256.h>
+#include <bitops.h>
+#include <proc.h>
+#include <export.h>
 
-static const uint32_t K[64] =
-{
-	0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
-	0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
-	0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
-	0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
-	0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
-	0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
-	0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
-	0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
-	0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
-	0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
-	0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
-	0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
-	0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
-	0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
-	0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
-	0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2
+static const uint32_t sha256_k[64] = {
+    0x428a2f98, 0x71374491, 0xb5c0fbcf, 0xe9b5dba5,
+    0x3956c25b, 0x59f111f1, 0x923f82a4, 0xab1c5ed5,
+    0xd807aa98, 0x12835b01, 0x243185be, 0x550c7dc3,
+    0x72be5d74, 0x80deb1fe, 0x9bdc06a7, 0xc19bf174,
+    0xe49b69c1, 0xefbe4786, 0x0fc19dc6, 0x240ca1cc,
+    0x2de92c6f, 0x4a7484aa, 0x5cb0a9dc, 0x76f988da,
+    0x983e5152, 0xa831c66d, 0xb00327c8, 0xbf597fc7,
+    0xc6e00bf3, 0xd5a79147, 0x06ca6351, 0x14292967,
+    0x27b70a85, 0x2e1b2138, 0x4d2c6dfc, 0x53380d13,
+    0x650a7354, 0x766a0abb, 0x81c2c92e, 0x92722c85,
+    0xa2bfe8a1, 0xa81a664b, 0xc24b8b70, 0xc76c51a3,
+    0xd192e819, 0xd6990624, 0xf40e3585, 0x106aa070,
+    0x19a4c116, 0x1e376c08, 0x2748774c, 0x34b0bcb5,
+    0x391c0cb3, 0x4ed8aa4a, 0x5b9cca4f, 0x682e6ff3,
+    0x748f82ee, 0x78a5636f, 0x84c87814, 0x8cc70208,
+    0x90befffa, 0xa4506ceb, 0xbef9a3f7, 0xc67178f2,
 };
 
-static void sha256_transform(struct sha256_ctx_t * ctx)
+#define S0(x) (ror32(x,  7) ^ ror32(x, 18) ^ (x >>  3))
+#define S1(x) (ror32(x, 17) ^ ror32(x, 19) ^ (x >> 10))
+#define E0(x) (ror32(x,  2) ^ ror32(x, 13) ^ ror32(x, 22))
+#define E1(x) (ror32(x,  6) ^ ror32(x, 11) ^ ror32(x, 25))
+#define CH(x, y, z) (z ^ (x & (y ^ z)))
+#define MAJ(x, y, z) ((x & y) | (z & (x | y)))
+
+#define SHA256_LOAD(c, b, d) (b[c] = unaligned_get_be32((uint32_t *)d + c))
+#define SHA256_BLEND(c, b) (b[c] = S1(b[c - 2]) + b[c - 7] + S0(b[c - 15]) + b[c - 16])
+#define SHA256_ROUND(c, b, A, B, C, D, E, F, G, H) do {     \
+    uint32_t tmp1, tmp2;                                    \
+    tmp1 = H + E1(E) + CH(E, F, G) + sha256_k[c] + b[c];    \
+    tmp2 = E0(A) + MAJ(A, B, C);                            \
+    D += tmp1; H = tmp1 + tmp2;                             \
+} while (0)
+
+void sha256_transform(uint32_t *digest, uint32_t *buff, const char *data)
 {
-	uint32_t W[64];
-	uint32_t A, B, C, D, E, F, G, H;
-	uint8_t * p = ctx->buf;
-	int t;
+    uint32_t A, B, C, D, E, F, G, H;
+    unsigned int count;
 
-	for(t = 0; t < 16; ++t)
-	{
-		uint32_t tmp =  *p++ << 24;
-		tmp |= *p++ << 16;
-		tmp |= *p++ << 8;
-		tmp |= *p++;
-		W[t] = tmp;
-	}
+    /* load state */
+    A = digest[0];
+    B = digest[1];
+    C = digest[2];
+    D = digest[3];
+    E = digest[4];
+    F = digest[5];
+    G = digest[6];
+    H = digest[7];
 
-	for(; t < 64; t++)
-	{
-		uint32_t s0 = ror(W[t-15], 7) ^ ror(W[t-15], 18) ^ shr(W[t-15], 3);
-		uint32_t s1 = ror(W[t-2], 17) ^ ror(W[t-2], 19) ^ shr(W[t-2], 10);
-		W[t] = W[t-16] + s0 + W[t-7] + s1;
-	}
+    /* load data */
+    for (count = 0; count < 16; count += 8) {
+        SHA256_LOAD(count + 0, buff, data);
+        SHA256_LOAD(count + 1, buff, data);
+        SHA256_LOAD(count + 2, buff, data);
+        SHA256_LOAD(count + 3, buff, data);
+        SHA256_LOAD(count + 4, buff, data);
+        SHA256_LOAD(count + 5, buff, data);
+        SHA256_LOAD(count + 6, buff, data);
+        SHA256_LOAD(count + 7, buff, data);
+    }
 
-	A = ctx->state[0];
-	B = ctx->state[1];
-	C = ctx->state[2];
-	D = ctx->state[3];
-	E = ctx->state[4];
-	F = ctx->state[5];
-	G = ctx->state[6];
-	H = ctx->state[7];
+    /* blend data */
+    for (count = 16; count < 64; count += 8) {
+        SHA256_BLEND(count + 0, buff);
+        SHA256_BLEND(count + 1, buff);
+        SHA256_BLEND(count + 2, buff);
+        SHA256_BLEND(count + 3, buff);
+        SHA256_BLEND(count + 4, buff);
+        SHA256_BLEND(count + 5, buff);
+        SHA256_BLEND(count + 6, buff);
+        SHA256_BLEND(count + 7, buff);
+    }
 
-	for(t = 0; t < 64; t++)
-	{
-		uint32_t s0 = ror(A, 2) ^ ror(A, 13) ^ ror(A, 22);
-		uint32_t maj = (A & B) ^ (A & C) ^ (B & C);
-		uint32_t t2 = s0 + maj;
-		uint32_t s1 = ror(E, 6) ^ ror(E, 11) ^ ror(E, 25);
-		uint32_t ch = (E & F) ^ ((~E) & G);
-		uint32_t t1 = H + s1 + ch + K[t] + W[t];
+    /* iterate register */
+    for (count = 0; count < 64; count += 8) {
+        SHA256_ROUND(count + 0, buff, A, B, C, D, E, F, G, H);
+        SHA256_ROUND(count + 1, buff, H, A, B, C, D, E, F, G);
+        SHA256_ROUND(count + 2, buff, G, H, A, B, C, D, E, F);
+        SHA256_ROUND(count + 3, buff, F, G, H, A, B, C, D, E);
+        SHA256_ROUND(count + 4, buff, E, F, G, H, A, B, C, D);
+        SHA256_ROUND(count + 5, buff, D, E, F, G, H, A, B, C);
+        SHA256_ROUND(count + 6, buff, C, D, E, F, G, H, A, B);
+        SHA256_ROUND(count + 7, buff, B, C, D, E, F, G, H, A);
+    }
 
-		H = G;
-		G = F;
-		F = E;
-		E = D + t1;
-		D = C;
-		C = B;
-		B = A;
-		A = t1 + t2;
-	}
-
-	ctx->state[0] += A;
-	ctx->state[1] += B;
-	ctx->state[2] += C;
-	ctx->state[3] += D;
-	ctx->state[4] += E;
-	ctx->state[5] += F;
-	ctx->state[6] += G;
-	ctx->state[7] += H;
+    /* update state */
+    digest[0] += A;
+    digest[1] += B;
+    digest[2] += C;
+    digest[3] += D;
+    digest[4] += E;
+    digest[5] += F;
+    digest[6] += G;
+    digest[7] += H;
 }
+EXPORT_SYMBOL(sha256_transform);
 
-void sha256_init(struct sha256_ctx_t * ctx)
+void sha224_transform(uint32_t *digest, uint32_t *buff, const char *data)
 {
-    ctx->state[0] = 0x6a09e667;
-    ctx->state[1] = 0xbb67ae85;
-    ctx->state[2] = 0x3c6ef372;
-    ctx->state[3] = 0xa54ff53a;
-    ctx->state[4] = 0x510e527f;
-    ctx->state[5] = 0x9b05688c;
-    ctx->state[6] = 0x1f83d9ab;
-    ctx->state[7] = 0x5be0cd19;
-    ctx->count = 0;
+    sha256_transform(digest, buff, data);
 }
+EXPORT_SYMBOL(sha224_transform);
 
-void sha256_update(struct sha256_ctx_t * ctx, const void * data, int len)
+void sha256_init(uint32_t *digest)
 {
-	int i = (int)(ctx->count & 63);
-	const uint8_t * p = (const uint8_t *)data;
-
-	ctx->count += len;
-	while(len--)
-	{
-		ctx->buf[i++] = *p++;
-		if(i == 64)
-		{
-			sha256_transform(ctx);
-			i = 0;
-		}
-	}
+    digest[0] = SHA256_DIGEST0;
+    digest[1] = SHA256_DIGEST1;
+    digest[2] = SHA256_DIGEST2;
+    digest[3] = SHA256_DIGEST3;
+    digest[4] = SHA256_DIGEST4;
+    digest[5] = SHA256_DIGEST5;
+    digest[6] = SHA256_DIGEST6;
+    digest[7] = SHA256_DIGEST7;
 }
+EXPORT_SYMBOL(sha256_init);
 
-const uint8_t * sha256_final(struct sha256_ctx_t * ctx)
+void sha224_init(uint32_t *digest)
 {
-	uint8_t * p = ctx->buf;
-	uint64_t cnt = ctx->count * 8;
-	int i;
-
-	sha256_update(ctx, (uint8_t *)"\x80", 1);
-	while ((ctx->count & 63) != 56)
-	{
-		sha256_update(ctx, (uint8_t *)"\0", 1);
-	}
-	for(i = 0; i < 8; ++i)
-	{
-		uint8_t tmp = (uint8_t)(cnt >> ((7 - i) * 8));
-		sha256_update(ctx, &tmp, 1);
-	}
-
-	for(i = 0; i < 8; i++)
-	{
-		uint32_t tmp = ctx->state[i];
-		*p++ = tmp >> 24;
-		*p++ = tmp >> 16;
-		*p++ = tmp >> 8;
-		*p++ = tmp >> 0;
-	}
-
-	return ctx->buf;
+    digest[0] = SHA224_DIGEST0;
+    digest[1] = SHA224_DIGEST1;
+    digest[2] = SHA224_DIGEST2;
+    digest[3] = SHA224_DIGEST3;
+    digest[4] = SHA224_DIGEST4;
+    digest[5] = SHA224_DIGEST5;
+    digest[6] = SHA224_DIGEST6;
+    digest[7] = SHA224_DIGEST7;
 }
-
-/*
- * Compute sha256 (256-bits) message digest
- */
-const uint8_t * sha256_hash(const void * data, int len, uint8_t * digest)
-{
-	struct sha256_ctx_t ctx;
-	sha256_init(&ctx);
-	sha256_update(&ctx, data, len);
-	memcpy(digest, sha256_final(&ctx), SHA256_DIGEST_SIZE);
-	return digest;
-}
+EXPORT_SYMBOL(sha224_init);
