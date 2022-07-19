f55787f4ea078caba850bb627b9e322e48f8d229
John Sanpe <sanpeqf@gmail.com>
2022-07-20 05:08:54 +0800

    Function: Added read/write lock api in spinlock.

--- include/spinlock.h
+++ include/spinlock.h
@@ -65,4 +65,124 @@ static __always_inline int spin_trylock_irqsave(spinlock_t *lock, irqflags_t *ir
     return raw_spin_trylock_irqsave(&lock->rlock, irqflags);
 }
 
-#endif	/* _SPINLOCK_H_ */
+static __always_inline void read_lock(rwlock_t *lock)
+{
+    raw_read_lock(&lock->rlock);
+}
+
+static __always_inline void read_unlock(rwlock_t *lock)
+{
+    raw_read_unlock(&lock->rlock);
+}
+
+static __always_inline void read_lock_bh(rwlock_t *lock)
+{
+    raw_read_lock_bh(&lock->rlock);
+}
+
+static __always_inline void read_unlock_bh(rwlock_t *lock)
+{
+    raw_read_unlock_bh(&lock->rlock);
+}
+
+static __always_inline void read_lock_irq(rwlock_t *lock)
+{
+    raw_read_lock_irq(&lock->rlock);
+}
+
+static __always_inline void read_unlock_irq(rwlock_t *lock)
+{
+    raw_read_unlock_irq(&lock->rlock);
+}
+
+static __always_inline void read_lock_irqsave(rwlock_t *lock, irqflags_t *irqflags)
+{
+    raw_read_lock_irqsave(&lock->rlock, irqflags);
+}
+
+static __always_inline void read_unlock_irqrestore(rwlock_t *lock, irqflags_t *irqflags)
+{
+    raw_read_unlock_irqrestore(&lock->rlock, irqflags);
+}
+
+static __always_inline int read_trylock(rwlock_t *lock)
+{
+    return raw_read_trylock(&lock->rlock);
+}
+
+static __always_inline int read_trylock_bh(rwlock_t *lock)
+{
+    return raw_read_trylock_bh(&lock->rlock);
+}
+
+static __always_inline int read_trylock_irq(rwlock_t *lock)
+{
+    return raw_read_trylock_irq(&lock->rlock);
+}
+
+static __always_inline int read_trylock_irqsave(rwlock_t *lock, irqflags_t *irqflags)
+{
+    return raw_read_trylock_irqsave(&lock->rlock, irqflags);
+}
+
+static __always_inline void write_lock(rwlock_t *lock)
+{
+    raw_write_lock(&lock->rlock);
+}
+
+static __always_inline void write_unlock(rwlock_t *lock)
+{
+    raw_write_unlock(&lock->rlock);
+}
+
+static __always_inline void write_lock_bh(rwlock_t *lock)
+{
+    raw_write_lock_bh(&lock->rlock);
+}
+
+static __always_inline void write_unlock_bh(rwlock_t *lock)
+{
+    raw_write_unlock_bh(&lock->rlock);
+}
+
+static __always_inline void write_lock_irq(rwlock_t *lock)
+{
+    raw_write_lock_irq(&lock->rlock);
+}
+
+static __always_inline void write_unlock_irq(rwlock_t *lock)
+{
+    raw_write_unlock_irq(&lock->rlock);
+}
+
+static __always_inline void write_lock_irqsave(rwlock_t *lock, irqflags_t *irqflags)
+{
+    raw_write_lock_irqsave(&lock->rlock, irqflags);
+}
+
+static __always_inline void write_unlock_irqrestore(rwlock_t *lock, irqflags_t *irqflags)
+{
+    raw_write_unlock_irqrestore(&lock->rlock, irqflags);
+}
+
+static __always_inline int write_trylock(rwlock_t *lock)
+{
+    return raw_write_trylock(&lock->rlock);
+}
+
+static __always_inline int write_trylock_bh(rwlock_t *lock)
+{
+    return raw_write_trylock_bh(&lock->rlock);
+}
+
+static __always_inline int write_trylock_irq(rwlock_t *lock)
+{
+    return raw_write_trylock_irq(&lock->rlock);
+}
+
+static __always_inline int write_trylock_irqsave(rwlock_t *lock, irqflags_t *irqflags)
+{
+    return raw_write_trylock_irqsave(&lock->rlock, irqflags);
+}
+
+#endif  /* _SPINLOCK_H_ */
