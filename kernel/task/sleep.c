e894596d8b85f1042a56eebe4300b15b9176f31e
John Sanpe <sanpeqf@gmail.com>
2022-07-25 13:57:49 +0800

    Function: Added ticktime min and max for sched.

--- kernel/task/sleep.c
+++ kernel/task/sleep.c
@@ -3,8 +3,12 @@
  * Copyright(c) 2021 Sanpe <sanpeqf@gmail.com>
  */
 
+#define MODULE_NAME "sleep"
+#define pr_fmt(fmt) MODULE_NAME ": " fmt
+
 #include <sched.h>
 #include <timer.h>
+#include <printk.h>
 #include <export.h>
 
 static void timeout_wakeup(void *pdata)
@@ -17,18 +21,23 @@ long sched_timeout(ttime_t timeout)
 {
     ttime_t expire;
 
-    if (timeout == ULONG_MAX) {
+    if (timeout == TTIME_MAX) {
         sched_yield();
-        return timeout;
-    }
+        goto finish;
+    } else if (timeout < 0) {
+		pr_err("wrong timeout value %lld\n", timeout);
+        current_set_state(SCHED_TASK_RUNNING);
+        goto finish;
+	}
 
-    expire = ticktime + timeout;
-
+    expire = timeout + ticktime;
     DEFINE_TIMER(timer, timeout_wakeup, current, timeout, 0);
     timer_pending(&timer);
-
     sched_yield();
-    return expire < ticktime ? 0 : expire - ticktime;
+    timeout = expire - ticktime;
+
+finish:
+    return max(timeout, 0);
 }
 EXPORT_SYMBOL(sched_timeout);
 
@@ -49,8 +58,8 @@ EXPORT_SYMBOL(sched_timeout_uninterruptible);
 void sched_msleep(unsigned long msec)
 {
     ttime_t timeout = ttime_to_ms(msec);
-    do {
+    do
         timeout = sched_timeout_uninterruptible(timeout);
-    } while (timeout);
+    while (timeout);
 }
 EXPORT_SYMBOL(sched_msleep);
