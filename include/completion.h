2db085fa454752c8a0c4be37d45fb139931e74c4
John Sanpe <sanpeqf@gmail.com>
2022-08-04 20:28:50 +0800

    Function: Added a simple completion implementation.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/completion.h
@@ -0,0 +1,34 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _COMPLETION_H_
+#define _COMPLETION_H_
+
+#include <waitqueue.h>
+
+struct completion {
+    struct wait_queue_head queue;
+    unsigned int counter;
+};
+
+#define COMPLETION_STATIC(name) {               \
+    .queue = WAITQUEUE_STATIC((name).queue),    \
+    .counter = 0,                               \
+}
+
+#define COMPLETION_INIT(name) \
+    (struct completion) COMPLETION_STATIC(name)
+
+#define DEFINE_COMPLETION(name) \
+    struct completion name = COMPLETION_INIT(name)
+
+static inline void completion_init(struct completion *comp)
+{
+    *comp = COMPLETION_INIT(*comp);
+}
+
+extern ttime_t completion_wait(struct completion *comp);
+extern ttime_t completion_wait_timeout(struct completion *comp, ttime_t timeout);
+extern void complete_one(struct completion *comp);
+extern void complete_all(struct completion *comp);
+extern bool completion_done(struct completion *comp);
+
+#endif  /* _COMPLETION_H_ */
---
+++ kernel/sched/completion.c
@@ -0,0 +1,79 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <completion.h>
+#include <waitqueue.h>
+#include <sched.h>
+#include <export.h>
+
+static ttime_t commin_wait(struct completion *comp, unsigned long state, ttime_t timeout)
+{
+    struct wait_queue node;
+
+    if (!comp->counter) {
+        do {
+            waitqueue_wait_state_locked(&comp->queue, &node, state);
+            spin_unlock_irq(&comp->queue.lock);
+            timeout = sched_timeout(timeout);
+            spin_lock_irq(&comp->queue.lock);
+        } while (!comp->counter && timeout);
+
+        if (!comp->counter)
+            return timeout;
+    }
+
+    if (comp->counter != UINT_MAX)
+        comp->counter--;
+
+    return timeout;
+}
+
+ttime_t completion_wait(struct completion *comp)
+{
+    return commin_wait(comp, SCHED_TASK_UNINTERRUPTIBLE, TTIME_MAX);
+}
+EXPORT_SYMBOL(completion_wait);
+
+ttime_t completion_wait_timeout(struct completion *comp, ttime_t timeout)
+{
+    return commin_wait(comp, SCHED_TASK_UNINTERRUPTIBLE, timeout);
+}
+EXPORT_SYMBOL(completion_wait_timeout);
+
+void complete_one(struct completion *comp)
+{
+    irqflags_t irqflags;
+
+    spin_lock_irqsave(&comp->queue.lock, &irqflags);
+    if (comp->counter != UINT_MAX)
+        comp->counter++;
+    waitqueue_wakeup_one_locked(&comp->queue);
+    spin_unlock_irqrestore(&comp->queue.lock, &irqflags);
+}
+EXPORT_SYMBOL(complete_one);
+
+void complete_all(struct completion *comp)
+{
+    irqflags_t irqflags;
+
+    spin_lock_irqsave(&comp->queue.lock, &irqflags);
+    comp->counter = UINT_MAX;
+    waitqueue_wakeup_all_locked(&comp->queue);
+    spin_unlock_irqrestore(&comp->queue.lock, &irqflags);
+}
+EXPORT_SYMBOL(complete_all);
+
+bool completion_done(struct completion *comp)
+{
+    irqflags_t flags;
+
+    if (!READ_ONCE(comp->counter))
+        return false;
+
+    spin_lock_irqsave(&comp->queue.lock, &flags);
+    spin_unlock_irqrestore(&comp->queue.lock, &flags);
+    return true;
+}
+EXPORT_SYMBOL(completion_done);
