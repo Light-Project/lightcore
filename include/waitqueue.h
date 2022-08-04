42ee016eb68ffb35c1aada753c94874428fef76f
John Sanpe <sanpeqf@gmail.com>
2022-08-04 20:25:29 +0800

    Function: Added a simple waitqueue implementation.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/waitqueue.h
+++ include/waitqueue.h
@@ -5,18 +5,44 @@
 #include <spinlock.h>
 #include <list.h>
 
-struct waitqueue_entry;
-typedef state (*waitqueue_func_t)(struct waitqueue_entry *entry, void *pdata);
+struct sched_task;
 
-struct waitqueue_head {
+struct wait_queue_head {
     spinlock_t lock;
     struct list_head waits;
 };
 
-struct waitqueue_entry {
+struct wait_queue {
     struct list_head list;
-    unsigned long flags;
-    void *pdata;
+    struct sched_task *task;
 };
+
+#define WAITQUEUE_STATIC(name) {                \
+    .waits = LIST_HEAD_STATIC((name).waits),    \
+    .lock = SPIN_LOCK_STATIC(name),             \
+}
+
+#define WAITQUEUE_INIT(name) \
+    (struct wait_queue_head) WAITQUEUE_STATIC(name)
+
+#define DEFINE_WAITQUEUE(name) \
+    struct wait_queue_head name = WAITQUEUE_INIT(name)
+
+static inline void waitqueue_init(struct wait_queue_head *queue)
+{
+    *queue = WAITQUEUE_INIT(*queue);
+}
+
+/* Wait queue enqueue */
+extern void waitqueue_wait_prepare_locked(struct wait_queue_head *queue, struct wait_queue *node);
+extern void waitqueue_wait_state_locked(struct wait_queue_head *queue, struct wait_queue *node, unsigned long state);
+extern void waitqueue_wait_prepare(struct wait_queue_head *queue, struct wait_queue *node);
+extern void waitqueue_wait_state(struct wait_queue_head *queue, struct wait_queue *node, unsigned long state);
+
+/* Wait queue dequeue */
+extern void waitqueue_wakeup_one_locked(struct wait_queue_head *queue);
+extern void waitqueue_wakeup_all_locked(struct wait_queue_head *queue);
+extern void waitqueue_wakeup_one(struct wait_queue_head *queue);
+extern void waitqueue_wakeup_all(struct wait_queue_head *queue);
 
 #endif  /* _WAITQUEUE_H_ */
