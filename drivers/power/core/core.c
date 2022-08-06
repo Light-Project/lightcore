01b1df978667672e8b6c5deef7501c0e5713bb46
John Sanpe <sanpeqf@gmail.com>
2022-08-06 23:55:05 +0800

    Maintain: Added power device support for multi-threaded registration.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- drivers/power/core/core.c
+++ drivers/power/core/core.c
@@ -7,8 +7,10 @@
 #define pr_fmt(fmt) MODULE_NAME ": " fmt
 
 #include <driver/power.h>
+#include <export.h>
 
 static LIST_HEAD(power_list);
+static SPIN_LOCK(power_lock);
 
 /**
  * power_shutdown - warm restart by power device.
@@ -18,13 +20,16 @@ void power_restart(void)
 {
     struct power_device *pdev;
 
+    spin_lock(&power_lock);
     list_for_each_entry(pdev, &power_list, list) {
         if (pdev->ops->restart) {
             pr_emerg("trying to restart using '%s'\n", pdev->dev->name);
             pdev->ops->restart(pdev);
         }
     }
+    spin_unlock(&power_lock);
 }
+EXPORT_SYMBOL(power_restart);
 
 /**
  * power_shutdown - cold reset by power device.
@@ -34,13 +39,16 @@ void power_reset(void)
 {
     struct power_device *pdev;
 
+    spin_lock(&power_lock);
     list_for_each_entry(pdev, &power_list, list) {
         if (pdev->ops->reset) {
             pr_emerg("trying to reset using '%s'\n", pdev->dev->name);
             pdev->ops->reset(pdev);
         }
     }
+    spin_unlock(&power_lock);
 }
+EXPORT_SYMBOL(power_reset);
 
 /**
  * power_shutdown - shutdown by power device.
@@ -50,13 +58,16 @@ void power_shutdown(void)
 {
     struct power_device *pdev;
 
+    spin_lock(&power_lock);
     list_for_each_entry(pdev, &power_list, list) {
         if (pdev->ops->shutdown) {
             pr_emerg("trying to shutdown using '%s'\n", pdev->dev->name);
             pdev->ops->shutdown(pdev);
         }
     }
+    spin_unlock(&power_lock);
 }
+EXPORT_SYMBOL(power_shutdown);
 
 /**
  * power_register - register a power device.
@@ -67,11 +78,14 @@ state power_register(struct power_device *pdev)
     if (!pdev->ops || !pdev->dev)
         return -EINVAL;
 
-    pr_debug("register device '%s'\n", pdev->dev->name);
+    spin_lock(&power_lock);
     list_add(&power_list, &pdev->list);
+    spin_unlock(&power_lock);
+    pr_debug("register device '%s'\n", pdev->dev->name);
 
     return -ENOERR;
 }
+EXPORT_SYMBOL(power_register);
 
 /**
  * power_unregister - unregister a power device.
@@ -79,6 +93,9 @@ state power_register(struct power_device *pdev)
  */
 void power_unregister(struct power_device *pdev)
 {
-    pr_debug("unregister device '%s'\n", pdev->dev->name);
+    spin_lock(&power_lock);
     list_del(&pdev->list);
+    spin_unlock(&power_lock);
+    pr_debug("unregister device '%s'\n", pdev->dev->name);
 }
+EXPORT_SYMBOL(power_unregister);
