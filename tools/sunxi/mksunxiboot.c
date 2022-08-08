cb577dc60fa592550a70f6ef865a6cb4c67112b1
John Sanpe <sanpeqf@gmail.com>
2022-08-08 20:58:06 +0800

    Maintain: Fixed the compatibility problem of mksunxi's upper computer.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- tools/sunxi/mksunxiboot.c
+++ tools/sunxi/mksunxiboot.c
@@ -2,36 +2,32 @@
 #include <string.h>
 #include <stdint.h>
 #include <stdlib.h>
+#include <endian.h>
 
-#define __ALIGN_MASK(x, mask)	(((x) + (mask)) & ~(mask))
-#define ALIGN(x, a)		__ALIGN_MASK((x), (typeof(x))(a) - 1)
+#define __ALIGN_MASK(x, mask) (((x) + (mask)) & ~(mask))
+#define ALIGN(x, a) __ALIGN_MASK((x), (typeof(x))(a) - 1)
 
-#if 0
-static inline uint32_t __swab32(uint32_t x)
-{
-    return ( (x<<24) | (x>>24) | \
-        ((x & (uint32_t)0x0000ff00UL)<<8) | \
-        ((x & (uint32_t)0x00ff0000UL)>>8) );
-}
-#define cpu_to_le32(x)		(__swab32((uint32_t)(x)))
-#define le32_to_cpu(x)		(__swab32((uint32_t)(x)))
-#else
-#define cpu_to_le32(x)		(x)
-#define le32_to_cpu(x)		(x)
+#if BYTE_ORDER == LITTLE_ENDIAN
+#define le32_to_cpu(val) (val)
+#define cpu_to_le32(val) (val)
+#endif
+#if BYTE_ORDER == BIG_ENDIAN
+#define le32_to_cpu(val) __bswap_32(val)
+#define cpu_to_le32(val) __bswap_32(val)
 #endif
 
 struct sunxi_head {
-    uint32_t    instruction;
-    uint8_t     magic[8];
-    uint32_t    checksum;
-    uint32_t    length;
-    uint8_t     spl_signature[4];
-    uint32_t    fel_script_address;
-    uint32_t    fel_uenv_length;
-    uint32_t    dt_name_offset;
-    uint32_t    reserved1;
-    uint32_t    boot_media;
-    uint32_t    string_pool[13];
+    uint32_t instruction;
+    uint8_t magic[8];
+    uint32_t checksum;
+    uint32_t length;
+    uint8_t spl_signature[4];
+    uint32_t fel_script_address;
+    uint32_t fel_uenv_length;
+    uint32_t dt_name_offset;
+    uint32_t reserved1;
+    uint32_t boot_media;
+    uint32_t string_pool[13];
 };
 
 int main(int argc, char *argv[])
@@ -67,7 +63,6 @@ int main(int argc, char *argv[])
 
     buflen = ALIGN(filelen, 512);
     buffer = malloc(buflen);
-
     memset(buffer, 0, buflen);
 
     if (fread(buffer, 1, filelen, fp) != filelen) {
@@ -89,7 +84,6 @@ int main(int argc, char *argv[])
         sum += le32_to_cpu(p[i]);
 
     h->checksum = cpu_to_le32(sum);
-
     fseek(fp, 0L, SEEK_SET);
 
     if (fwrite(buffer, 1, buflen, fp) != buflen) {
