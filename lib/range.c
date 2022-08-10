7b149e922b0627af38ca3afc88461c6c1a7c89ec
John Sanpe <sanpeqf@gmail.com>
2022-08-10 23:36:58 +0800

    Maintain: Added adjustment type of range library.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- lib/range.c
+++ lib/range.c
@@ -4,6 +4,7 @@
  */
 
 #include <range.h>
+#include <overflow.h>
 #include <export.h>
 
 static __always_inline struct range_node *
@@ -15,7 +16,16 @@ range_node_alloc(struct range_head *head)
 static __always_inline void
 range_node_free(struct range_head *head, struct range_node *node)
 {
+    list_del(&node->list);
     head->free(node, head->pdata);
+}
+
+static __always_inline void
+range_node_copy(struct range_head *head, struct range_node *dest,
+                const struct range_node *src)
+{
+    if (head->copy)
+        head->copy(dest, src);
 }
 
 /**
@@ -24,12 +34,12 @@ range_node_free(struct range_head *head, struct range_node *node)
  * @type: the type of insert range.
  * @start: the start of insert range.
  * @size: the size of insert range.
- * @expel: forced insertion of range by expulsion.
+ * @resize: range sizing rules.
  * @merge: merge adjacent nodes of the same type.
  */
 struct range_node *range_insert(struct range_head *head, unsigned int type,
                                 unsigned long start, unsigned long size,
-                                bool expel, bool merge)
+                                enum range_resize resize, bool merge)
 {
     struct range_node *block, *next, *new, *tmp, *last = NULL;
     struct list_head *prev = NULL;
@@ -40,53 +50,112 @@ struct range_node *range_insert(struct range_head *head, unsigned int type,
         return new;
 
     list_for_each_entry_safe(block, next, &head->nodes, list) {
-        unsigned long end, block_end;
-        end = start + size - 1;
-        block_end = block->start + block->size - 1;
+        unsigned long end, blkend;
 
-        if (start <= block->start && block_end <= end) {
+        /* prevent value overflow */
+        end = overflow_add(start, size - 1);
+        blkend = overflow_add(block->start, block->size - 1);
+
+        if (start <= block->start && blkend <= end) {
             /* completely cover */
-            if (unlikely(!expel)) {
-                retval = -EALREADY;
+            if (unlikely(resize == RANGE_COLLISION)) {
+                retval = -EADDRINUSE;
                 goto failed;
             }
 
-            range_node_free(head, block);
-        } else if (start <= block->start && block->start < end) {
+            if (type == block->type)
+                range_node_free(head, block);
+            else {
+                if (resize == RANGE_DISLODGE)
+                    range_node_free(head, block);
+                else {
+                    if (start != block->start && blkend != end) {
+                        tmp = range_node_alloc(head);
+                        if (unlikely(!tmp)) {
+                            retval = -ENOENT;
+                            goto failed;
+                        }
+
+                        tmp->start = start;
+                        tmp->size = block->start - start;
+                        tmp->type = type;
+
+                        start = blkend + 1;
+                        size = end - blkend;
+                        prev = &block->list;
+
+                        range_node_copy(head, tmp, new);
+                        list_add_prev(prev, &tmp->list);
+                    }
+
+                    else if (start != block->start)
+                        goto retraction_font;
+
+                    else if (blkend != end)
+                        goto retraction_back;
+
+                    else {
+                        retval = -ENODATA;
+                        goto failed;
+                    }
+                }
+            }
+        } else if (start <= block->start && block->start <= end) {
             /* front cover */
-            if (unlikely(!expel)) {
-                retval = -EALREADY;
+            if (unlikely(resize == RANGE_COLLISION)) {
+                retval = -EADDRINUSE;
                 goto failed;
             }
 
             if (type == block->type) {
-                size += block_end - end;
+                size += blkend - end;
                 range_node_free(head, block);
             } else {
-                block->start = end + 1;
-                block->size -= size;
+                if (resize == RANGE_DISLODGE) {
+                    block->start = end + 1;
+                    block->size = blkend - block->start + 1;
+                } else {
+                    if (start != block->start) {
+                    retraction_font:
+                        size = block->start - start;
+                    } else {
+                        retval = -ENODATA;
+                        goto failed;
+                    }
+                }
             }
             break;
-        } else if (start < block_end && block_end <= end) {
+        } else if (start <= blkend && blkend <= end) {
             /* back cover */
-            if (unlikely(!expel)) {
-                retval = -EALREADY;
+            if (unlikely(resize == RANGE_COLLISION)) {
+                retval = -EADDRINUSE;
                 goto failed;
             }
 
             if (type == block->type) {
                 start = block->start;
-                size += start - block->start;
+                size = end - start + 1;
                 prev = block->list.prev;
                 range_node_free(head, block);
             } else {
-                block->size = start - block->start;
-                prev = &block->list;
+                if (resize == RANGE_DISLODGE) {
+                    block->size = start - block->start;
+                    prev = &block->list;
+                } else {
+                    if (blkend != end) {
+                    retraction_back:
+                        start = blkend + 1;
+                        size = end - start + 1;
+                    } else {
+                        retval = -ENODATA;
+                        goto failed;
+                    }
+                }
             }
-        } else if (block->start < start && end < block_end) {
+        } else if (block->start < start && end < blkend) {
             /* middle cover */
-            if (unlikely(!expel)) {
-                retval = -EALREADY;
+            if (unlikely(resize == RANGE_COLLISION)) {
+                retval = -EADDRINUSE;
                 goto failed;
             }
 
@@ -95,21 +164,26 @@ struct range_node *range_insert(struct range_head *head, unsigned int type,
                 size = block->size;
                 range_node_free(head, block);
             } else {
-                tmp = range_node_alloc(head);
-                if (PTR_INVAL_ZERO(tmp)) {
-                    retval = PTR_INVAL(tmp);
+                if (resize == RANGE_DISLODGE) {
+                    tmp = range_node_alloc(head);
+                    if (unlikely(!tmp)) {
+                        retval = -ENOENT;
+                        goto failed;
+                    }
+
+                    block->size = start - block->start;
+                    prev = &block->list;
+
+                    tmp->start = end + 1;
+                    tmp->size = blkend - end;
+                    tmp->type = block->type;
+
+                    range_node_copy(head, tmp, block);
+                    list_add(prev, &tmp->list);
+                } else {
+                    retval = -ENODATA;
                     goto failed;
                 }
-
-                block->size = start - block->start;
-                prev = &block->list;
-
-                tmp->start = end + 1;
-                tmp->size = block_end - end;
-                tmp->type = block->type;
-                if (head->copy)
-                    head->copy(tmp, block);
-                list_add(prev, &tmp->list);
             }
             break;
         }
@@ -127,13 +201,13 @@ struct range_node *range_insert(struct range_head *head, unsigned int type,
     if (!prev) {
         if (likely(last))
             prev = &last->list;
-        else  /* first node */
+        else /* first node */
             prev = &head->nodes;
     }
     list_add(prev, &new->list);
 
     if (merge) {
-        /* merge the prev same attribute node */
+        /* merge the prev same type adjacent node */
         if (prev != &head->nodes) {
             tmp = list_prev_entry(new, list);
             if (type == tmp->type && tmp->start + tmp->size == start) {
@@ -143,7 +217,7 @@ struct range_node *range_insert(struct range_head *head, unsigned int type,
             }
         }
 
-        /* merge the next same attribute node */
+        /* merge the next same type adjacent node */
         tmp = list_next_entry_or_null(new, &head->nodes, list);
         if (tmp && type == tmp->type && start + size == tmp->start) {
             new->size += tmp->size;
@@ -154,7 +228,7 @@ struct range_node *range_insert(struct range_head *head, unsigned int type,
     return new;
 
 failed:
-    range_node_free(head, new);
+    head->free(new, head->pdata);
     return ERR_PTR(retval);
 }
 EXPORT_SYMBOL(range_insert);
@@ -170,29 +244,34 @@ state range_subtract(struct range_head *head, unsigned long start, unsigned long
     struct range_node *block, *tmp;
 
     list_for_each_entry(block, &head->nodes, list) {
-        unsigned long end, block_end;
-        end = start + size;
-        block_end = block->start + block->size;
+        unsigned long end, blkend;
 
-        if (start <= block->start && block_end <= end) {
+        end = overflow_add(start, size - 1);
+        blkend = overflow_add(block->start, block->size - 1);
+
+        if (start <= block->start && blkend <= end) {
             /* completely cover */
             range_node_free(head, block);
-        } else if (start < block->start && block->start < end) {
+        } else if (start <= block->start && block->start <= end) {
             /* front cover */
-            block->start = end;
+            block->start = end + 1;
+            block->size = blkend - block->start + 1;
             break;
-        } else if (start < block_end && block_end < end) {
+        } else if (start < blkend && blkend < end) {
             /* back cover */
-            block->size = block_end - start;
-        } else if (block->start < start && end < block_end) {
+            block->size = start - block->start;
+        } else if (block->start < start && end < blkend) {
             /* middle cover */
             tmp = range_node_alloc(head);
             if (PTR_INVAL_ZERO(tmp))
                 return PTR_INVAL(tmp);
 
-            tmp->start = end;
-            tmp->size = block_end - end;
             block->start = end;
+            list_add(&block->list, &tmp->list);
+
+            tmp->start = end;
+            tmp->size = blkend - end;
+            range_node_copy(head, tmp, block);
             break;
         }
     }
@@ -201,39 +280,11 @@ state range_subtract(struct range_head *head, unsigned long start, unsigned long
 }
 EXPORT_SYMBOL(range_subtract);
 
-/**
- * range_find - find the first range within a interval.
- * @head: the range head to find.
- * @type: the range type to find.
- * @align: start address alignment requirements of range.
- * @start: start address of the find range.
- * @size: size of the find range.
- */
-struct range_node *range_find(struct range_head *head, unsigned int type,
-                              unsigned long align, unsigned long start, unsigned long size)
+void range_release(struct range_head *head)
 {
-    struct range_node *tmp, *block = NULL;
-    unsigned long aligned;
+    struct range_node *node, *tmp;
 
-    list_for_each_entry(tmp, &head->nodes, list) {
-        aligned = align_high(tmp->start, align);
-
-        if (tmp->type != type || tmp->start < start)
-            continue;
-
-        if (start + size < tmp->start)
-            break;
-
-        /* Prevent overflow caused by too large address. */
-        if (aligned + size < aligned || aligned < tmp->start)
-            continue;
-
-        if (aligned + size <= tmp->start + tmp->size) {
-            block = tmp;
-            break;
-        }
-    }
-
-    return block;
+    list_for_each_entry_safe(node, tmp, &head->nodes, list)
+        range_node_free(head, node);
 }
-EXPORT_SYMBOL(range_find);
+EXPORT_SYMBOL(range_release);
