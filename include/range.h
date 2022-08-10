849b8eb533d01f9d790942db8bd1af2ceb002709
John Sanpe <sanpeqf@gmail.com>
2022-08-10 23:36:58 +0800

    Maintain: Added adjustment type of range library.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/range.h
+++ include/range.h
@@ -8,6 +8,12 @@ typedef struct range_node *(*range_alloc_t)(void *pdata);
 typedef void (*range_free_t)(struct range_node *node, void *pdata);
 typedef void (*range_copy_t)(struct range_node *dest, const struct range_node *src);
 
+enum range_resize {
+    RANGE_COLLISION     = 0,
+    RANGE_DISLODGE      = 1,
+    RANGE_RETRACTION    = 2,
+};
+
 struct range_head {
     struct list_head nodes;
     range_alloc_t alloc;
@@ -23,8 +29,26 @@ struct range_node {
     unsigned int type;
 };
 
-extern struct range_node *range_find(struct range_head *head, unsigned int type, unsigned long align, unsigned long start, unsigned long size);
-extern struct range_node *range_insert(struct range_head *head, unsigned int type, unsigned long start, unsigned long size, bool expel, bool merge);
+#define RANGE_STATIC(name, _alloc, _free, _copy, _pdata) {  \
+    .nodes = LIST_HEAD_STATIC((name).nodes),                \
+    .alloc = _alloc, .free = _free,                         \
+    .copy = _copy, .pdata = _pdata,                         \
+}
+
+#define RANGE_INIT(name, alloc, free, copy, pdata) \
+    (struct range_head) RANGE_STATIC(name, alloc, free, copy, pdata)
+
+#define DEFINE_RANGE(name, alloc, free, copy, pdata) \
+    struct range_head name = RANGE_INIT(name, alloc, free, copy, pdata)
+
+static inline void range_init(struct range_head *head, range_alloc_t alloc,
+                              range_free_t free, range_copy_t copy, void *pdata)
+{
+    *head = RANGE_INIT(*head, alloc, free, copy, pdata);
+}
+
+extern struct range_node *range_insert(struct range_head *head, unsigned int type, unsigned long start, unsigned long size, enum range_resize resize, bool merge);
 extern state range_subtract(struct range_head *head, unsigned long start, unsigned long size);
+extern void range_release(struct range_head *head);
 
 #endif  /* _RANGE_H_ */
