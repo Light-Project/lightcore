a196f689fbdd1e4f3bd38abca4b4fe2468718b6a
John Sanpe <sanpeqf@gmail.com>
2022-08-11 04:41:36 +0800

    Maintain: Use newly developed range library to manage memblocks.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- mm/memblock.c
+++ mm/memblock.c
@@ -14,7 +14,11 @@
 #define MEMBLOCK_REGIONS CONFIG_MEMBLK_REGIONS
 static bool memblock_merge = IS_ENABLED(CONFIG_MEMBLK_MERGE);
 static struct memblock_region memblock_regions[MEMBLOCK_REGIONS];
-static LIST_HEAD(memblock_list);
+
+static __init struct range_node *memblock_node_alloc(void *pdata);
+static __init void memblock_node_remove(struct range_node *range, void *pdata);
+static __init void memblock_node_copy(struct range_node *dest, const struct range_node *src);
+static DEFINE_RANGE(memblock_range, memblock_node_alloc, memblock_node_remove, memblock_node_copy, NULL);
 
 #ifdef CONFIG_MEMBLK_DBG
 # define memblk_debug(fmt, ...) pr_debug(fmt, ##__VA_ARGS__)
@@ -44,194 +48,83 @@ static inline int memblock_node_get_numa(struct memblock_region *reg)
 }
 #endif
 
-static inline __init struct memblock_region *memblock_node_alloc(void)
+static __init struct range_node *memblock_node_alloc(void *pdata)
 {
     for (int count = 0; count < MEMBLOCK_REGIONS; ++count) {
-        if (!memblock_regions[count].use) {
-            memblock_regions[count].use = true;
-            return &memblock_regions[count];
+        if (!memblock_regions[count].used) {
+            memblock_regions[count].used = true;
+            return &memblock_regions[count].range;
         }
     }
 
     return NULL;
 }
 
-static inline __init void memblock_node_remove(struct memblock_region *region)
+static __init void
+memblock_node_remove(struct range_node *range, void *pdata)
 {
-    list_del(&region->list);
-    region->use = false;
+    struct memblock_region *region = range_to_memblock(range);
+    region->used = false;
+}
+
+static __init void
+memblock_node_copy(struct range_node *dest, const struct range_node *src)
+{
+    struct memblock_region *rdest = range_to_memblock(dest);
+    const struct memblock_region *rsrc = range_to_memblock(src);
+    rdest->name = rsrc->name;
 }
 
 static __init struct memblock_region *
-memblock_insert(phys_addr_t addr, size_t size, enum memblock_type type, bool merge)
+memblock_insert(phys_addr_t addr, size_t size,
+                enum memblock_type type, bool merge)
 {
-    struct memblock_region *block, *next, *new, *tmp, *last = NULL;
-    struct list_head *prev = NULL;
-
-    new = memblock_node_alloc();
-    if (unlikely(!new))
-        return ERR_PTR(-ENOMEM);
-
-    list_for_each_entry_safe(block, next, &memblock_list, list) {
-        phys_addr_t end, block_end;
-        end = addr + size - 1;
-        block_end = block->addr + block->size - 1;
-
-        if (addr <= block->addr && block_end <= end) {
-            /* completely cover */
-            memblock_node_remove(block);
-        } else if (addr <= block->addr && block->addr < end) {
-            /* front cover */
-            if (type == block->type) {
-                size += block_end - end;
-                memblock_node_remove(block);
-            } else {
-                block->addr = end + 1;
-                block->size -= size;
-            }
-            break;
-        } else if (addr < block_end && block_end <= end) {
-            /* back cover */
-            if (type == block->type) {
-                addr = block->addr;
-                size += addr - block->addr;
-                prev = block->list.prev;
-                memblock_node_remove(block);
-            } else {
-                block->size = addr - block->addr;
-                prev = &block->list;
-            }
-        } else if (block->addr < addr && end < block_end) {
-            /* middle cover */
-            if (type == block->type) {
-                addr = block->addr;
-                size = block->size;
-                memblock_node_remove(block);
-            } else {
-                tmp = memblock_node_alloc();
-                if (unlikely(!tmp))
-                    goto free;
-
-                block->size = addr - block->addr;
-                prev = &block->list;
-
-                tmp->addr = end + 1;
-                tmp->size = block_end - end;
-                tmp->type = block->type;
-                tmp->name = block->name;
-                list_add(prev, &tmp->list);
-            }
-            break;
-        }
-
-        if (!prev && block->addr < addr)
-            last = block;
-    }
-
-    /* init new node */
-    new->addr = addr;
-    new->size = size;
-    new->type = type;
-
-    /* insert new node */
-    if (!prev) {
-        if (likely(last))
-            prev = &last->list;
-        else  /* first node */
-            prev = &memblock_list;
-    }
-    list_add(prev, &new->list);
-
-    if (merge) {
-        /* merge the prev same attribute node */
-        if (prev != &memblock_list) {
-            tmp = list_prev_entry(new, list);
-            if (type == tmp->type && tmp->addr + tmp->size == addr) {
-                new->addr = tmp->addr;
-                new->size += tmp->size;
-                memblock_node_remove(tmp);
-            }
-        }
-
-        /* merge the next same attribute node */
-        tmp = list_next_entry_or_null(new, &memblock_list, list);
-        if (tmp && type == tmp->type && addr + size == tmp->addr) {
-            new->size += tmp->size;
-            memblock_node_remove(tmp);
-        }
-    }
-
-    return new;
-
-free:
-    memblock_node_remove(new);
-    return ERR_PTR(-ENOMEM);
+    struct range_node *range;
+    range = range_insert(&memblock_range, type, addr, size,
+                         RANGE_DISLODGE, merge);
+    return range_to_memblock(range);
 }
 
-static __init state memblock_clear(phys_addr_t addr, size_t size)
+static __init state
+memblock_clear(phys_addr_t addr, size_t size)
 {
-    struct memblock_region *block, *tmp;
-
-    list_for_each_entry(block, &memblock_list, list) {
-        phys_addr_t end, block_end;
-        end = addr + size;
-        block_end = block->addr + block->size;
-
-        if (addr <= block->addr && block_end <= end) {
-            /* completely cover */
-            memblock_node_remove(block);
-        } else if (addr < block->addr && block->addr < end) {
-            /* front cover */
-            block->addr = end;
-            break;
-        } else if (addr < block_end && block_end < end) {
-            /* back cover */
-            block->size = block_end - addr;
-        } else if (block->addr < addr && end < block_end) {
-            /* middle cover */
-            block->addr = end;
-            tmp = memblock_node_alloc();
-            if (!tmp)
-                return -ENOMEM;
-            tmp->addr = end;
-            tmp->size = block_end - end;
-            break;
-        }
-    }
-
-    return -ENOERR;
+    return range_subtract(&memblock_range, addr, size);
 }
 
-phys_addr_t __init memblock_phys_alloc_range(size_t size, size_t align,
-                    phys_addr_t min_addr, phys_addr_t max_addr, int numa)
+phys_addr_t __init
+memblock_phys_alloc_range(size_t size, size_t align, phys_addr_t min_addr,
+                          phys_addr_t max_addr, int numa)
 {
     struct memblock_region *tmp, *block = NULL;
     phys_addr_t align_start;
 
-    if (!size)
-        return -EINVAL;
+    if (unlikely(!size))
+        return 0;
 
-    list_for_each_entry(tmp, &memblock_list, list) {
-        align_start = align_high(tmp->addr, align);
+    list_for_each_entry(tmp, &memblock_range.nodes, range.list) {
+        align_start = align_high(tmp->range.start, align);
 
-        if (tmp->type != MEMBLOCK_USABLE || tmp->addr < min_addr)
+        if (tmp->range.type != MEMBLOCK_USABLE ||
+            tmp->range.start < min_addr)
             continue;
 
-        if (max_addr < tmp->addr)
+        if (max_addr < tmp->range.start)
             break;
 
         /* Prevent overflow caused by too large address. */
         if (align_start + size < align_start ||
-            align_start < tmp->addr)
+            align_start < tmp->range.start)
             continue;
 
-        if (align_start + size <= tmp->addr + tmp->size) {
+        if (align_start + size <= tmp->range.start +
+            tmp->range.size) {
             block = tmp;
             break;
         }
-    } if (unlikely(!block)) {
-        return 0;
     }
+
+    if (unlikely(!block))
+        return 0;
 
     block = memblock_insert(align_start, size, MEMBLOCK_ALLOCATED, true);
     if (unlikely(IS_INVAL(block))) {
@@ -243,8 +136,8 @@ phys_addr_t __init memblock_phys_alloc_range(size_t size, size_t align,
     return align_start;
 }
 
-static void __init *memblock_alloc_internal(size_t size, size_t align,
-                    phys_addr_t min_addr, phys_addr_t max_addr, int numa)
+static void __init *memblock_alloc_internal(size_t size, size_t align, phys_addr_t min_addr,
+                                            phys_addr_t max_addr, int numa)
 {
     phys_addr_t phys;
 
@@ -258,8 +151,8 @@ static void __init *memblock_alloc_internal(size_t size, size_t align,
     return pa_to_va(phys);
 }
 
-void __init *memblock_alloc_range(size_t size, size_t align,
-                    phys_addr_t min_addr, phys_addr_t max_addr, int numa)
+void __init *memblock_alloc_range(size_t size, size_t align, phys_addr_t min_addr,
+                                  phys_addr_t max_addr, int numa)
 {
     void *block;
 
@@ -274,10 +167,11 @@ void __init *memblock_alloc_range(size_t size, size_t align,
 void __init memblock_takeover(enum memblock_type type, bool (*fn)(struct memblock_region *))
 {
     struct memblock_region *block;
-    list_for_each_entry(block, &memblock_list, list) {
-        if (block->type == type)
+
+    list_for_each_entry(block, &memblock_range.nodes, range.list) {
+        if (block->range.type == type)
         if (fn(block))
-            block->type = MEMBLOCK_TAKEOVER;
+            block->range.type = MEMBLOCK_TAKEOVER;
     }
 }
 
@@ -286,7 +180,7 @@ state __init memblock_add_numa(const char *name, phys_addr_t addr, size_t size, 
     phys_addr_t end = addr + size - 1;
     struct memblock_region *node;
 
-    if (!size)
+    if (unlikely(!size))
         return -EINVAL;
 
     addr = align_high(addr, MSIZE);
@@ -302,6 +196,7 @@ state __init memblock_add_numa(const char *name, phys_addr_t addr, size_t size, 
 
     node->name = name;
     memblock_node_set_numa(node, numa);
+
     return -ENOERR;
 }
 
@@ -310,7 +205,7 @@ state __init memblock_reserve(const char *name, phys_addr_t addr, size_t size)
     phys_addr_t end = addr + size - 1;
     struct memblock_region *node;
 
-    if (!size)
+    if (unlikely(!size))
         return -EINVAL;
 
     addr = align_low(addr, MSIZE);
@@ -326,6 +221,7 @@ state __init memblock_reserve(const char *name, phys_addr_t addr, size_t size)
 
     node->name = name;
     memblock_node_set_numa(node, 0);
+
     return -ENOERR;
 }
 
@@ -333,7 +229,7 @@ state __init memblock_remove(phys_addr_t addr, size_t size)
 {
     phys_addr_t end = addr + size - 1;
 
-    if (!size)
+    if (unlikely(!size))
         return -EINVAL;
 
     memblk_debug("clear [%#018lx - %#018lx]\n", addr, end);
@@ -345,9 +241,9 @@ void __init memblock_dump(void)
     struct memblock_region *node;
 
     pr_info("memblock layout:\n");
-    list_for_each_entry(node, &memblock_list, list) {
-        phys_addr_t end = node->addr + node->size - 1;
+    list_for_each_entry(node, &memblock_range.nodes, range.list) {
+        phys_addr_t end = node->range.start + node->range.size - 1;
         pr_info("  [%#018lx-%#018lx] %s type: %d\n",
-                node->addr, end, node->name, node->type);
+                node->range.start, end, node->name, node->range.type);
     }
 }
