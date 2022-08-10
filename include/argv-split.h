68c9ef19a9f544d23994ab2cf64163dafc0e306d
John Sanpe <sanpeqf@gmail.com>
2022-08-10 17:45:11 +0800

    Function: Added argv-merge library.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/argv-split.h
+++ include/argv-split.h
@@ -6,5 +6,6 @@
 
 extern unsigned int argv_count(const char *string);
 extern char **argv_split(const char *string, unsigned int *argcp, gfp_t flags);
+extern int argv_merge(int argc, char *argv[], char *buff, int size);
 
 #endif  /* _ARGV_SPLIT_H_ */
