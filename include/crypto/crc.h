13f849553bc4b08184d40f1d5aef419c4786082c
John Sanpe <sanpeqf@gmail.com>
2022-08-15 03:37:49 +0800

    Function: Added crc64 and crc-rocksoft to crypto library.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/crypto/crc.h
+++ include/crypto/crc.h
@@ -8,10 +8,12 @@ extern uint8_t crc4(const uint8_t *src, size_t len, uint8_t crc);
 extern uint8_t crc8(const uint8_t *src, size_t len, uint8_t crc);
 extern uint16_t crc16(const uint8_t *src, size_t len, uint16_t crc);
 extern uint32_t crc32(const uint8_t *src, size_t len, uint32_t crc);
+extern uint64_t crc64(const uint8_t *src, size_t len, uint64_t crc);
 
 extern uint16_t crc_ccitt(const uint8_t *src, size_t len, uint16_t crc);
 extern uint16_t crc_ccittf(const uint8_t *src, size_t len, uint16_t crc);
 extern uint16_t crc_itut(const uint8_t *src, size_t len, uint16_t crc);
 extern uint16_t crc_t10dif(const uint8_t *src, size_t len, uint16_t crc);
+extern uint64_t crc_rocksoft(const uint8_t *src, size_t len, uint64_t crc);
 
 #endif  /* _CRYPTO_CRC_H_ */
