c5de5b1f16bb12b18d0a304000118321222eb00a
John Sanpe <sanpeqf@gmail.com>
2022-08-18 11:07:07 +0800

    Function: Added shortest path find/insert to skiplist library.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/skiplist.h
+++ include/skiplist.h
@@ -6,6 +6,8 @@
 
 typedef long (*skiplist_find_t)(const void *data, const void *key);
 typedef long (*skiplist_cmp_t)(const void *dataa, const void *datab);
+typedef unsigned long (*skiplist_even_find_t)(const void *data, const void *key, bool *signplus);
+typedef unsigned long (*skiplist_even_cmp_t)(const void *dataa, const void *datab, bool *signplus);
 
 struct skip_node {
     void *pdata;
@@ -23,6 +25,10 @@ struct skip_head {
 extern state skiplist_insert(struct skip_head *head, void *data, skiplist_cmp_t cmp);
 extern void skiplist_delete(struct skip_head *head, void *key, skiplist_find_t find);
 extern void *skiplist_find(struct skip_head *head, void *key, skiplist_find_t find);
+
+extern state skiplist_even_insert(struct skip_head *head, void *data, skiplist_even_cmp_t cmp);
+extern void skiplist_even_delete(struct skip_head *head, void *key, skiplist_even_find_t find);
+extern void *skiplist_even_find(struct skip_head *head, void *key, skiplist_even_find_t find);
 
 extern void skiplist_reset(struct skip_head *head);
 extern struct skip_head *skiplist_create(unsigned int levels, gfp_t flags);
