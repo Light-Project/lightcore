e4f3ca049b757540b962b218984ab60b092d734c
John Sanpe <sanpeqf@gmail.com>
2022-09-07 15:26:43 +0800

    Function: Add tsc command support for kshell on x86.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ usr/command/arch/x86-tsc.c
@@ -0,0 +1,56 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <initcall.h>
+#include <kshell.h>
+#include <asm/regs.h>
+
+static void usage(struct kshell_context *ctx)
+{
+    kshell_printf(ctx, "usage: tsc\n");
+    kshell_printf(ctx, "\t-h  display this message\n");
+}
+
+static state tsc_main(struct kshell_context *ctx, int argc, char *argv[])
+{
+    unsigned int count;
+    uint64_t value;
+
+    for (count = 1; count < argc; ++count) {
+        char *para = argv[count];
+
+        if (para[0] != '-')
+            break;
+
+        switch (para[1]) {
+            case 'h': default:
+                goto usage;
+        }
+    }
+
+    if (argc != count)
+        goto usage;
+
+    value = tsc_get();
+    kshell_printf(ctx, "rdtsc: %#018llx\n", value);
+
+    return -ENOERR;
+
+usage:
+    usage(ctx);
+    return -EINVAL;
+}
+
+static struct kshell_command tsc_cmd = {
+    .name = "tsc",
+    .desc = "show processor tsc",
+    .exec = tsc_main,
+};
+
+static state tsc_init(void)
+{
+    return kshell_register(&tsc_cmd);
+}
+kshell_initcall(tsc_init);
