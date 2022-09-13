08edae82a99267303170d436e00dba5e33cfefa4
John-sanpe <sanpeqf@gmail.com>
2022-09-13 22:32:45 +0800

    Function: Add ctz and clz functions to bitops.
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

--- include/asm-generic/bitops.h
+++ include/asm-generic/bitops.h
@@ -251,9 +251,9 @@ static inline bool bit_atomic_test_flip(volatile unsigned long *addr, long bit)
 #endif
 
 /**
- * ffsuf - find first set bit in word (unsafe)
- * @val: The word to search
- * Undefined if no bit exists, so code should check against 0 first.
+ * ffsuf - find first set bit in word (unsafe).
+ * @val: The word to search.
+ * Undefined if no bit exists, so code should check against 0 first..
  */
 #ifndef ffsuf
 static inline unsigned int ffsuf(unsigned long value)
@@ -296,9 +296,9 @@ static inline unsigned int ffsuf(unsigned long value)
 #endif
 
 /*
- * flsuf: find last set bit in word (unsafe)
- * @val: The word to search
- * Undefined if no set bit exists, so code should check against 0 first.
+ * flsuf: find last set bit in word (unsafe).
+ * @val: The word to search.
+ * Undefined if no set bit exists, so code should check against 0 first..
  */
 #ifndef flsuf
 static inline unsigned int flsuf(unsigned long value)
@@ -336,9 +336,9 @@ static inline unsigned int flsuf(unsigned long value)
 #endif
 
 /*
- * ffzuf - find first zero in word (unsafe)
- * @word: The word to search
- * Undefined if no zero exists, so code should check against ~0UL first.
+ * ffzuf - find first zero in word (unsafe).
+ * @word: The word to search.
+ * Undefined if no zero exists, so code should check against ~0UL first..
  */
 #ifndef ffzuf
 static inline unsigned int ffzuf(unsigned long value)
@@ -348,9 +348,9 @@ static inline unsigned int ffzuf(unsigned long value)
 #endif
 
 /*
- * flzuf - find last zero in word (unsafe)
- * @word: The word to search
- * Undefined if no zero exists, so code should check against ~0UL first.
+ * flzuf - find last zero in word (unsafe).
+ * @word: The word to search.
+ * Undefined if no zero exists, so code should check against ~0UL first..
  */
 #ifndef flzuf
 static inline unsigned int flzuf(unsigned long value)
@@ -360,8 +360,8 @@ static inline unsigned int flzuf(unsigned long value)
 #endif
 
 /**
- * ffs - find first bit set
- * @value: the word to search
+ * ffs - find first bit set.
+ * @value: the word to search.
  */
 #ifndef ffs
 static inline unsigned int ffs(unsigned long value)
@@ -373,8 +373,8 @@ static inline unsigned int ffs(unsigned long value)
 #endif
 
 /**
- * fls - find last zero in word
- * @value: the word to search
+ * fls - find last zero in word.
+ * @value: the word to search.
  */
 #ifndef fls
 static inline unsigned int fls(unsigned long value)
@@ -386,8 +386,8 @@ static inline unsigned int fls(unsigned long value)
 #endif
 
 /*
- * ffz - find first zero in word
- * @value: The word to search
+ * ffz - find first zero in word.
+ * @value: The word to search.
  */
 #ifndef ffz
 static inline unsigned int ffz(unsigned long value)
@@ -399,8 +399,8 @@ static inline unsigned int ffz(unsigned long value)
 #endif
 
 /*
- * flz - find last zero in word
- * @value: The word to search
+ * flz - find last zero in word.
+ * @value: The word to search.
  */
 #ifndef flz
 static inline unsigned int flz(unsigned long value)
@@ -411,15 +411,45 @@ static inline unsigned int flz(unsigned long value)
 }
 #endif
 
+/*
+ * ctz - returns the number of trailing 0-bits in value.
+ * @value: The word to search.
+ */
+#ifndef ctz
+static inline unsigned int ctz(unsigned long value)
+{
+    if (value == ULONG_MAX)
+        return 0;
+    if (value == ULONG_MIN)
+        return BITS_PER_LONG;
+    return ffsuf(value);
+}
+#endif
+
+/*
+ * clz - returns the number of leading 0-bits in value.
+ * @value: The word to search.
+ */
+#ifndef clz
+static inline unsigned int clz(unsigned long value)
+{
+    if (value == ULONG_MAX)
+        return 0;
+    if (value == ULONG_MIN)
+        return BITS_PER_LONG;
+    return BITS_PER_LONG - 1 - flsuf(value);
+}
+#endif
+
 #if BITS_PER_LONG == 32
 static inline unsigned int ffsuf64(uint64_t value)
 {
     uint32_t hi = value >> 32;
 
     if ((uint32_t)value)
-        return ffs((uint32_t)value);
+        return ffsuf((uint32_t)value);
 
-    return ffs(hi) + 32;
+    return ffsuf(hi) + 32;
 }
 #else
 # define ffsuf64 ffsuf
@@ -437,6 +467,34 @@ static inline unsigned int flsuf64(uint64_t value)
 }
 #else
 # define flsuf64 flsuf
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int ffzuf64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if ((uint32_t)value)
+        return ffzuf((uint32_t)value);
+
+    return ffzuf(hi) + 32;
+}
+#else
+# define ffzuf64 ffzuf
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int flzuf64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if (hi)
+        return flzuf(hi) + 32;
+
+    return flzuf((uint32_t)value);
+}
+#else
+# define flzuf64 flzuf
 #endif
 
 #if BITS_PER_LONG == 32
@@ -465,6 +523,62 @@ static inline unsigned int fls64(uint64_t value)
 }
 #else
 # define fls64 fls
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int ffz64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if ((uint32_t)value)
+        return ffz((uint32_t)value);
+
+    return ffz(hi) + 32;
+}
+#else
+# define ffz64 ffz
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int flz64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if (hi)
+        return flz(hi) + 32;
+
+    return flz((uint32_t)value);
+}
+#else
+# define flz64 flz
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int ctz64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if ((uint32_t)value)
+        return ctz(value);
+
+    return ctz(hi) + 32;
+}
+#else
+# define ctz64 ctz
+#endif
+
+#if BITS_PER_LONG == 32
+static inline unsigned int clz64(uint64_t value)
+{
+    uint32_t hi = value >> 32;
+
+    if (hi)
+        return clz(hi);
+
+    return clz((uint32_t)value) + 32;
+}
+#else
+# define clz64 clz
 #endif
 
 #endif  /* __ASSEMBLY__ */
