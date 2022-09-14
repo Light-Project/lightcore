aabae5f021c0c40d6f8786abd07c999e1f72f28c
John-sanpe <sanpeqf@gmail.com>
2022-09-14 10:46:13 +0800

    Function: Added bitops acceleration support for gcc's builtin function.
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ include/asm-generic/builtin-ffs.h
@@ -0,0 +1,11 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _ASM_GENERIC_BUILTIN_FFS_H_
+#define _ASM_GENERIC_BUILTIN_FFS_H_
+
+#define ffsuf ffsuf
+static __always_inline unsigned int ffsuf(unsigned long value)
+{
+	return __builtin_ctzl(value);
+}
+
+#endif  /* _ASM_GENERIC_BUILTIN_FFS_H_ */
