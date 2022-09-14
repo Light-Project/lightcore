716f280f7066064620bebab93d3a9e3aa3cf7edb
John-sanpe <sanpeqf@gmail.com>
2022-09-14 11:02:23 +0800

    Function: Added bitops security acceleration support for gcc's builtin function.
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

---
+++ include/asm-generic/builtin-flsuf.h
@@ -0,0 +1,13 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _ASM_GENERIC_BUILTIN_FLSUF_H_
+#define _ASM_GENERIC_BUILTIN_FLSUF_H_
+
+#include <bits.h>
+
+#define flsuf flsuf
+static __always_inline unsigned int flsuf(unsigned long value)
+{
+	return BITS_PER_LONG - 1 - __builtin_clzl(value);
+}
+
+#endif  /* _ASM_GENERIC_BUILTIN_FLSUF_H_ */
