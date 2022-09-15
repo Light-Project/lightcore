66f2adcc655fec81de8077a19231e5ec6b01032a
John-sanpe <sanpeqf@gmail.com>
2022-09-15 16:01:46 +0800

    Maintain: Optimized respectively dynamic and static operations of popcount and parity.
    Signed-off-by: John-sanpe <sanpeqf@gmail.com>

--- include/popcount.h
+++ include/popcount.h
@@ -3,8 +3,13 @@
 #define _POPCOUNT_H_
 
 #include <types.h>
+#include <stddef.h>
 
-static inline unsigned int popcount8(uint8_t value)
+extern const uint8_t popcount_table[256];
+extern const uint8_t popparity_table[256];
+
+static __attribute_const__ __always_inline
+unsigned int popcount8_const(uint8_t value)
 {
     value = (value & 0x55) + ((value >> 1) & 0x55);
     value = (value & 0x33) + ((value >> 2) & 0x33);
@@ -12,7 +17,8 @@ static inline unsigned int popcount8(uint8_t value)
     return (unsigned int)value;
 }
 
-static inline unsigned int popcount16(uint16_t value)
+static __attribute_const__ __always_inline
+unsigned int popcount16_const(uint16_t value)
 {
     value = (value & 0x5555) + ((value >> 1) & 0x5555);
     value = (value & 0x3333) + ((value >> 2) & 0x3333);
@@ -21,7 +27,8 @@ static inline unsigned int popcount16(uint16_t value)
     return (unsigned int)value;
 }
 
-static inline unsigned int popcount32(uint32_t value)
+static __attribute_const__ __always_inline
+unsigned int popcount32_const(uint32_t value)
 {
     value = (value & 0x55555555UL) + ((value >>  1) & 0x55555555);
     value = (value & 0x33333333UL) + ((value >>  2) & 0x33333333);
@@ -31,7 +38,8 @@ static inline unsigned int popcount32(uint32_t value)
     return (unsigned int)value;
 }
 
-static inline unsigned int popcount64(uint64_t value)
+static __attribute_const__ __always_inline
+unsigned int popcount64_const(uint64_t value)
 {
     value = (value & 0x5555555555555555ULL) + ((value >>  1) & 0x5555555555555555ULL);
     value = (value & 0x3333333333333333ULL) + ((value >>  2) & 0x3333333333333333ULL);
@@ -42,24 +50,158 @@ static inline unsigned int popcount64(uint64_t value)
     return (unsigned int)value;
 }
 
-static inline bool popparity8(uint8_t value)
+static inline __attribute_const__
+bool popparity8_const(uint8_t value)
 {
-    return popcount8(value) & 0x01;
+    value ^= value >> 4;
+    value &= 0xf;
+    return (0x6996 >> value) & 1;
 }
 
-static inline bool popparity16(uint16_t value)
+static inline __attribute_const__
+bool popparity16_const(uint16_t value)
 {
-    return popcount16(value) & 0x01;
+    value ^= value >> 8;
+    value ^= value >> 4;
+    value &= 0xf;
+    return (0x6996 >> value) & 1;
 }
 
-static inline bool popparity32(uint32_t value)
+static inline __attribute_const__
+bool popparity32_const(uint32_t value)
 {
-    return popcount32(value) & 0x01;
+    value ^= value >> 16;
+    value ^= value >> 8;
+    value ^= value >> 4;
+    value &= 0xf;
+    return (0x6996 >> value) & 1;
 }
 
-static inline bool popparity64(uint64_t value)
+static inline __attribute_const__
+bool popparity64_const(uint64_t value)
 {
-    return popcount64(value) & 0x01;
+    value ^= value >> 32;
+    value ^= value >> 16;
+    value ^= value >> 8;
+    value ^= value >> 4;
+    value &= 0xf;
+    return (0x6996 >> value) & 1;
 }
+
+static __attribute_const__ inline
+unsigned int popcount8_dynamic(uint8_t value)
+{
+    return popcount_table[value];
+}
+
+static inline __attribute_const__
+unsigned int popcount16_dynamic(uint16_t value)
+{
+    uint8_t high = value >> 8;
+    return popcount8_dynamic(high) +
+           popcount8_dynamic((uint8_t)value);
+}
+
+static inline __attribute_const__
+unsigned int popcount32_dynamic(uint32_t value)
+{
+    uint16_t high = value >> 16;
+    return popcount16_dynamic(high) +
+           popcount16_dynamic((uint16_t)value);
+}
+
+static inline __attribute_const__
+unsigned int popcount64_dynamic(uint64_t value)
+{
+    uint32_t high = value >> 32;
+    return popcount32_dynamic(high) +
+           popcount32_dynamic((uint32_t)value);
+}
+
+static __attribute_const__ inline
+bool popparity8_dynamic(uint8_t value)
+{
+    return popparity_table[value];
+}
+
+static inline __attribute_const__
+bool popparity16_dynamic(uint16_t value)
+{
+    uint8_t high = value >> 8;
+    return (popparity8_dynamic((uint8_t)value) +
+            popparity8_dynamic(high)) & 1;
+}
+
+static inline __attribute_const__
+bool popparity32_dynamic(uint32_t value)
+{
+    uint16_t high = value >> 16;
+    return (popparity16_dynamic((uint16_t)value) +
+            popparity16_dynamic(high)) & 1;
+}
+
+static inline __attribute_const__
+bool popparity64_dynamic(uint64_t value)
+{
+    uint32_t high = value >> 32;
+    return (popparity32_dynamic((uint32_t)value) +
+            popparity32_dynamic(high)) & 1;
+}
+
+#define popcount8(value) ({         \
+    uint8_t __value = value;        \
+    __builtin_constant_p(__value)   \
+    ? popcount8_const(__value)      \
+    : popcount8_dynamic(__value);   \
+})
+
+#define popcount16(value) ({        \
+    uint16_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popcount16_const(__value)     \
+    : popcount16_dynamic(__value);  \
+})
+
+#define popcount32(value) ({        \
+    uint32_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popcount32_const(__value)     \
+    : popcount32_dynamic(__value);  \
+})
+
+#define popcount64(value) ({        \
+    uint64_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popcount64_const(__value)     \
+    : popcount64_dynamic(__value);  \
+})
+
+#define popparity8(value) ({        \
+    uint8_t __value = value;        \
+    __builtin_constant_p(__value)   \
+    ? popparity8_const(__value)     \
+    : popparity8_dynamic(__value);  \
+})
+
+#define popparity16(value) ({       \
+    uint16_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popparity16_const(__value)    \
+    : popparity16_dynamic(__value); \
+})
+
+#define popparity32(value) ({       \
+    uint32_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popparity32_const(__value)    \
+    : popparity32_dynamic(__value); \
+})
+
+#define popparity64(value) ({       \
+    uint64_t __value = value;       \
+    __builtin_constant_p(__value)   \
+    ? popparity64_const(__value)    \
+    : popparity64_dynamic(__value); \
+})
 
 #endif  /* _POPCOUNT_H_ */
