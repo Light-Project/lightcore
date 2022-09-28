83722c734f043b43e0b1a52be45ef0486d405a9d
John Sanpe <sanpeqf@gmail.com>
2022-09-28 14:40:43 +0800

    Supplement: Fixed inconsistency of gunit header files.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/gunit.h
+++ include/gunit.h
@@ -7,7 +7,7 @@
 #define GSIZE_BUFF sizeof("1023.45MiB")
 #define GFREQ_BUFF sizeof("1023.45GHz")
 
-extern int gunit(char *buff, uintmax_t value, const char *unit[], unsigned int units, size_t bsize);
+extern int gunit(char *buff, uintmax_t value, unsigned int step, const char *unit[], unsigned int units, size_t bsize);
 extern int gsize(char *buff, uintmax_t value);
 extern int gfreq(char *buff, uintmax_t value);
 
