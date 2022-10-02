6caefd83de4da3250b4ac212eedbe0799f017c26
John Sanpe <sanpeqf@gmail.com>
2022-10-02 17:40:50 +0800

    Function: Added string hash framework.
    [lib:stringhash] Initialization framework.
        -- Stringhash is a general string hash processing algorithm
            framework. It uses wordops to speed up the operation.
    
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ lib/stringhash.c
@@ -0,0 +1,28 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#include <stringhash.h>
+#include <proc.h>
+#include <asm/wordops.h>
+#include <export.h>
+
+uint64_t __weak stringhash(const char *str, unsigned long salt)
+{
+    const struct wordops constants = WORDOPS_CONSTANTS;
+    unsigned long value, temp, hash = 0;
+    intptr_t position;
+
+    position = 0;
+    value = unaligned_get_ulong(str);
+
+    while (!word_has_zero(&constants, &temp, value)) {
+        HASH_MIX(hash, salt, value);
+        position += BYTES_PER_LONG;
+        value = unaligned_get_ulong(str + position);
+    }
+
+    temp = word_prep_mask(&constants, temp, value);
+    temp = word_create_mask(temp);
+    hash ^= value & word_zero_mask(temp);
+
+    return strhash_create(fold_hash(hash, salt), word_find_zero(temp));
+}
+EXPORT_SYMBOL(stringhash);
