58a5833081df512391f8b7c7d5e0492e741469b2
John Sanpe <sanpeqf@gmail.com>
2022-10-02 16:21:43 +0800

    Function: Added golden ratio hash algorithm.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/hash.h
+++ include/hash.h
@@ -3,12 +3,39 @@
 #define _HASH_H_
 
 #include <types.h>
+#include <bits.h>
 
-#define GOLDEN_RATIO_32 0x61C88647U
-#define GOLDEN_RATIO_64 0x61C8864680B583EBULL
+#define GOLDEN_RATIO_32 0x61c88647UL
+#define GOLDEN_RATIO_64 0x61c8864680b583ebULL
 
-#define hashlen_create(hash, len)   ((uint64_t)(len << 32) | (uint32_t)(hash))
-#define hashlen_hash(hashlen)       ((uint32_t)((hashlen) >> 0))
-#define hashlen_len(hashlen)        ((uint32_t)((hashlen) >> 32))
+#ifdef CONFIG_ARCH_HAS_HASH
+# include <asm/hash.h>
+#endif
+
+#ifndef ARCH_HAS_HASH32
+# define __hash32 hash32_generic
+static __always_inline uint32_t hash32_generic(uint32_t value)
+{
+    return value * GOLDEN_RATIO_32;
+}
+#endif
+
+#ifndef ARCH_HAS_HASH64
+# define __hash64 hash64_generic
+static __always_inline uint64_t hash64_generic(uint64_t value)
+{
+    return value * GOLDEN_RATIO_64;
+}
+#endif
+
+static __always_inline uint32_t hash32(uint32_t value, unsigned int bits)
+{
+    return __hash32(value) >> (BITS_PER_U32 - bits);
+}
+
+static __always_inline uint32_t hash64(uint64_t value, unsigned int bits)
+{
+    return __hash64(value) >> (BITS_PER_U64 - bits);
+}
 
 #endif  /* _HASH_H_ */
