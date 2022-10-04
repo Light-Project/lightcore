3daa62a832bcf004cb93b814627266f0d80af77f
John Sanpe <sanpeqf@gmail.com>
2022-10-04 12:19:28 +0800

    Fcuntion: Added x86 earlycon input support for atkbd.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/driver/serio/ps2.h
+++ include/driver/serio/ps2.h
@@ -2,11 +2,12 @@
 #ifndef _DRIVER_PS2_H_
 #define _DRIVER_PS2_H_
 
-#define PS2_CMD_SEND(size) ((size) << 8)
-#define PS2_CMD_RECV(size) ((size) << 12)
-#define PS2_CMD(recv, send, cmd) (PS2_CMD_RECV(recv) | PS2_CMD_SEND(send) | cmd)
+#define PS2_CMD_SEND(size) (((size) & 0xf) << 8)
+#define PS2_CMD_RECV(size) (((size) & 0xf) << 12)
+#define PS2_CMD(recv, send, cmd) (PS2_CMD_RECV(recv) | PS2_CMD_SEND(send) | (cmd))
 
-#define ps2_cmd_send(cmd) (((cmd) >> 8) & 0xf)
-#define ps2_cmd_recv(cmd) (((cmd) >> 12) & 0xf)
+#define PS2_GET_CMD(cmd) ((cmd) & 0xff)
+#define PS2_GET_SEND(cmd) (((cmd) >> 8) & 0xf)
+#define PS2_GET_RECV(cmd) (((cmd) >> 12) & 0xf)
 
 #endif  /* _DRIVER_PS2_H_ */
