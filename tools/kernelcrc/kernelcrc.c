a25d6f5363ede0bda42e355291d818ac3014ae62
John Sanpe <sanpeqf@gmail.com>
2022-10-20 11:09:49 +0800

    Maintain: Optimize the code of kernelcrc tool.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- tools/kernelcrc/kernelcrc.c
+++ tools/kernelcrc/kernelcrc.c
@@ -8,20 +8,15 @@
 #include <stdlib.h>
 #include <stdint.h>
 #include <unistd.h>
+#include <errno.h>
+#include <err.h>
 #include <fcntl.h>
 #include <sys/mman.h>
 #include <sys/stat.h>
 
 #define be32 uint32_t
-
 #include "../../include/asm-generic/header.h"
 #include "../../include/crypto/crc32-table.h"
-
-static void error(const char *str)
-{
-    fprintf(stderr, "%s\n", str);
-    exit(1);
-}
 
 int main(int argc, char *argv[])
 {
@@ -31,40 +26,43 @@ int main(int argc, char *argv[])
     int fd;
 
     if ((fd = open(argv[1], O_RDWR)) < 0)
-        error("can not open file");
+        err(errno, "can not open file");
 
     if (fstat(fd, &stat) < 0)
-        error("fstat error");
+        err(errno, "fstat error");
 
     boot_head = mmap(NULL, stat.st_size, PROT_READ | PROT_WRITE, MAP_SHARED, fd, 0);
     if (boot_head == MAP_FAILED)
-        error("mmap error");
+        err(errno, "mmap error");
 
     if (strcmp("lightcore!", (char *)boot_head->magic)) {
         close(fd);
         remove(argv[1]);
-        error("magic error");
+        err(errno, "magic error");
     }
 
-    boot_head->size = stat.st_size - sizeof(struct boot_head);
     old_crc = boot_head->crc;
+    boot_head->size = stat.st_size - sizeof(struct boot_head);
     new_crc = crc32_inline((void *)(boot_head + 1), boot_head->size, ~0);
-    if (old_crc != new_crc) {
+
+    if (old_crc == new_crc)
+        printf("the kernel crc already exists");
+    else {
         boot_head->crc = new_crc;
         printf("kernel crc has been fixed: 0x%x\n", boot_head->crc);
-    } else
-        printf("the kernel crc already exists");
+    }
 
-    boot_head->hsize = sizeof(struct boot_head);
     old_crc = boot_head->hcrc;
     boot_head->hcrc = 0;
+    boot_head->hsize = sizeof(struct boot_head);
     new_crc = crc32_inline((void *)boot_head, sizeof(struct boot_head), ~0);
+
     if (old_crc != new_crc) {
+        boot_head->hcrc = old_crc;
+        printf("the header crc already exists\n");
+    } else {
         boot_head->hcrc = new_crc;
         printf("header crc has been fixed: 0x%x\n", boot_head->hcrc);
-    } else {
-        boot_head->hcrc = old_crc;
-        printf("the header crc already exists");
     }
 
     close(fd);
