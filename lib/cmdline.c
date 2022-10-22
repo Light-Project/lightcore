41b1bea82601a3b3d6c2c7c195031264773c53aa
John Sanpe <sanpeqf@gmail.com>
2022-10-22 19:59:35 +0800

    Function: Added cmdline library support for parsing escaped strings.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- lib/cmdline.c
+++ lib/cmdline.c
@@ -5,6 +5,7 @@
 
 #include <cmdline.h>
 #include <kernel.h>
+#include <ctype.h>
 #include <export.h>
 
 uint64_t memparse(const char *ptr, char **endptr)
@@ -49,3 +50,93 @@ uint64_t memparse(const char *ptr, char **endptr)
     return value;
 }
 EXPORT_SYMBOL(memparse);
+
+int escape_string(char *buff, size_t size, const char *fmt)
+{
+    char *str = buff;
+    char ch, tmp;
+
+    while ((ch = *fmt++) && size--) {
+        if (ch != '\\') {
+            *buff++ = ch;
+            continue;
+        }
+
+        switch (*fmt++) {
+            case 'a':
+                *buff++ = '\a';
+                break;
+
+            case 'b':
+                *buff++ = '\b';
+                break;
+
+            case 'e':
+                *buff++ = '\e';
+                break;
+
+            case 'f':
+                *buff++ = '\f';
+                break;
+
+            case 'n':
+                *buff++ = '\n';
+                break;
+
+            case 'r':
+                *buff++ = '\r';
+                break;
+
+            case 't':
+                *buff++ = '\t';
+                break;
+
+            case 'v':
+                *buff++ = '\v';
+                break;
+
+            case 'x':
+                if (!isxdigit(tmp = *fmt++))
+                    goto not_an_escape;
+
+                *buff = chtoi(tmp);
+                tmp = *fmt;
+
+                if (isxdigit(tmp)) {
+                    fmt++;
+                    *buff = *buff * 16 + chtoi(tmp);
+                }
+                buff++;
+                break;
+
+            case '0':
+                *buff++ = 0;
+                if (!('0' <= *fmt && *fmt <= '7'))
+                    break;
+                *--buff = *fmt++;
+                fallthrough;
+
+            case '1': case '2': case '3':
+            case '4': case '5': case '6': case '7':
+                *buff -= '0';
+                if ('0' <= *fmt && *fmt <= '7')
+                    *buff = *buff * 8 + (*fmt++ - '0');
+                if ('0' <= *fmt && *fmt <= '7')
+                    *buff = *buff * 8 + (*fmt++ - '0');
+                buff++;
+                break;
+
+            default: not_an_escape:
+                fmt--;
+                fallthrough;
+
+            case '\\':
+                *buff++ = '\\';
+                break;
+        }
+    }
+
+    *buff++ = '\0';
+    return buff - str;
+}
+EXPORT_SYMBOL(escape_string);
