82e7315560ed682c7b1957852956a2c63b516674
John Sanpe <sanpeqf@gmail.com>
2022-10-23 20:00:32 +0800

    Maintain: Added printk level selective compilation support.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/klevels.h
+++ include/klevels.h
@@ -16,36 +16,32 @@
 #define KERN_DEBUG      KERN_SOH "7"    /* debug-level messages */
 #define KERN_DEFAULT    ""              /* the default kernel loglevel */
 
-enum klevel {
-    KLEVEL_EMERG            = 0,
-    KLEVEL_ALERT            = 1,
-    KLEVEL_CRIT             = 2,
-    KLEVEL_ERR              = 3,
-    KLEVEL_WARNING          = 4,
-    KLEVEL_NOTICE           = 5,
-    KLEVEL_INFO             = 6,
-    KLEVEL_DEBUG            = 7,
-    KLEVEL_DEFAULT          = 8,
-};
+#define KLEVEL_EMERG            0
+#define KLEVEL_ALERT            1
+#define KLEVEL_CRIT             2
+#define KLEVEL_ERR              3
+#define KLEVEL_WARNING          4
+#define KLEVEL_NOTICE           5
+#define KLEVEL_INFO             6
+#define KLEVEL_DEBUG            7
+#define KLEVEL_DEFAULT          8
 
-enum klevel_color {
-    KERN_COLR_DEFAULT       = 39,
-    KERN_COLR_BLACK         = 30,
-    KERN_COLR_DARK_RED      = 31,
-    KERN_COLR_DARK_GREEN    = 32,
-    KERN_COLR_DARK_YELLOW   = 33,
-    KERN_COLR_DARK_BLUE     = 34,
-    KERN_COLR_DARK_MAGENTA  = 35,
-    KERN_COLR_DARK_CYAN     = 36,
-    KERN_COLR_LIGHT_GRAY    = 37,
-    KERN_COLR_DARK_GRAY     = 90,
-    KERN_COLR_RED           = 91,
-    KERN_COLR_GREEN         = 92,
-    KERN_COLR_YELLOW        = 93,
-    KERN_COLR_BLUE          = 94,
-    KERN_COLR_MAGENTA       = 95,
-    KERN_COLR_CYAN          = 96,
-    KERN_COLR_WHITE         = 97,
-};
+#define KERN_COLR_DEFAULT       39
+#define KERN_COLR_BLACK         30
+#define KERN_COLR_DARK_RED      31
+#define KERN_COLR_DARK_GREEN    32
+#define KERN_COLR_DARK_YELLOW   33
+#define KERN_COLR_DARK_BLUE     34
+#define KERN_COLR_DARK_MAGENTA  35
+#define KERN_COLR_DARK_CYAN     36
+#define KERN_COLR_LIGHT_GRAY    37
+#define KERN_COLR_DARK_GRAY     90
+#define KERN_COLR_RED           91
+#define KERN_COLR_GREEN         92
+#define KERN_COLR_YELLOW        93
+#define KERN_COLR_BLUE          94
+#define KERN_COLR_MAGENTA       95
+#define KERN_COLR_CYAN          96
+#define KERN_COLR_WHITE         97
 
 #endif /* _KLEVELS_H_ */
