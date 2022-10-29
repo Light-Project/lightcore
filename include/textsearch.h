7d16eed5b73998733120988f05a42ee6bbb08eae
John Sanpe <sanpeqf@gmail.com>
2022-10-29 20:20:06 +0800

    Function: Added initial text search framework with kmp algorithm.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ include/textsearch.h
@@ -0,0 +1,105 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+#ifndef _TEXTSEARCH_H_
+#define _TEXTSEARCH_H_
+
+#include <list.h>
+#include <error.h>
+#include <bitflags.h>
+#include <mm/gfp.h>
+
+enum ts_flags {
+    __TS_IGNORECASE = 0,
+};
+
+#define TS_IGNORECASE BIT(__TS_IGNORECASE)
+
+/**
+ * struct ts_state - search state.
+ * @offset: offset for next match.
+ * @offset: search context private data.
+ */
+struct ts_state {
+    unsigned int offset;
+    void *pdata;
+};
+
+struct ts_linear {
+    struct ts_state tss;
+    const char *data;
+    unsigned int len;
+};
+
+/**
+ * struct ts_context - search context.
+ * @ops: chosen search algorithm.
+ * @flags: user flags.
+ * @next_block: fetch next block of data.
+ */
+struct ts_context {
+    struct ts_algorithm *algo;
+    unsigned long flags;
+    unsigned int (*next_block)(struct ts_context *tsc, struct ts_state *tss,
+                               unsigned int consumed, const void **dest);
+};
+
+/**
+ * struct ts_algorithm - search algorithm operations.
+ * @name: name of search algorithm.
+ * @prepare: initialization function to prepare a search.
+ * @destroy: destroy algorithm specific parts of a search configuration.
+ * @find: find the next occurrence of the pattern.
+ * @pattern_get: return pointer of pattern.
+ * @pattern_len: return length of pattern.
+ */
+struct ts_algorithm {
+    struct list_head list;
+    const char *name;
+
+    struct ts_context *(*prepare)(const void *pattern, size_t len, gfp_t gfp, unsigned long flags);
+    void (*destroy)(struct ts_context *tsc);
+    unsigned int (*find)(struct ts_context *tsc, struct ts_state *tss);
+    const char *(*pattern_get)(struct ts_context *tsc);
+    unsigned int (*pattern_len)(struct ts_context *tsc);
+};
+
+GENERIC_STRUCT_BITOPS(ts, struct ts_context, flags)
+GENERIC_STRUCT_FLAG(ts, struct ts_context, flags, ignorecase, __TS_IGNORECASE)
+
+static inline unsigned int textsearch_find(struct ts_context *tsc, struct ts_state *tss)
+{
+    struct ts_algorithm *algo = tsc->algo;
+    tss->offset = 0;
+    return algo->find(tsc, tss);
+}
+
+static inline unsigned int textsearch_next(struct ts_context *tsc, struct ts_state *tss)
+{
+    struct ts_algorithm *algo = tsc->algo;
+    return algo->find(tsc, tss);
+}
+
+static inline const char *textsearch_pattern_get(struct ts_context *tsc)
+{
+    struct ts_algorithm *algo = tsc->algo;
+    return algo->pattern_get(tsc);
+}
+
+static inline unsigned int textsearch_pattern_len(struct ts_context *tsc)
+{
+    struct ts_algorithm *algo = tsc->algo;
+    return algo->pattern_len(tsc);
+}
+
+static inline void textsearch_destroy(struct ts_context *tsc)
+{
+    struct ts_algorithm *algo = tsc->algo;
+    algo->destroy(tsc);
+}
+
+extern struct ts_context *textsearch_prepare(const char *name, const void *pattern, size_t len, gfp_t gfp, unsigned long flags);
+extern unsigned int textsearch_linear_find(struct ts_context *tsc, struct ts_linear *linear, const void *data, unsigned int len);
+extern unsigned int textsearch_linear_next(struct ts_context *tsc, struct ts_linear *linear);
+extern state textsearch_register(struct ts_algorithm *algo);
+extern state textsearch_unregister(struct ts_algorithm *algo);
+
+#endif  /* _TEXTSEARCH_H_ */
---
+++ lib/textsearch/kmp.c
@@ -0,0 +1,120 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <ctype.h>
+#include <kmalloc.h>
+#include <initcall.h>
+#include <textsearch.h>
+
+struct kmp_context {
+    struct ts_context tsc;
+    char *pattern;
+    unsigned int pattern_len;
+    unsigned int prefix_table[0];
+};
+
+#define ts_to_kmp(ptr) \
+    container_of(ptr, struct kmp_context, tsc)
+
+static const char *kmp_pattern_get(struct ts_context *tsc)
+{
+    struct kmp_context *kctx = ts_to_kmp(tsc);
+    return kctx->pattern;
+}
+
+static unsigned int kmp_pattern_len(struct ts_context *tsc)
+{
+    struct kmp_context *kctx = ts_to_kmp(tsc);
+    return kctx->pattern_len;
+}
+
+static unsigned int kmp_find(struct ts_context *tsc, struct ts_state *tss)
+{
+    #define find_pattern() (icase ? toupper(text[index]) : text[index])
+    bool icase = ts_test_ignorecase(tsc);
+    struct kmp_context *kctx = ts_to_kmp(tsc);
+    unsigned int consumed = tss->offset;
+    unsigned int length, index, match = 0;
+    const char *text;
+
+    for (;;) {
+        length = tsc->next_block(tsc, tss, consumed, (const void **)&text);
+        if (unlikely(!length))
+            return UINT_MAX;
+
+        for (index = 0; index < length; ++index) {
+            while (match && kctx->pattern[match] != find_pattern())
+                match = kctx->prefix_table[match - 1];
+
+            if (kctx->pattern[match] == find_pattern())
+                match++;
+
+            if (unlikely(match == kctx->pattern_len)) {
+                tss->offset = consumed + index + 1;
+                return tss->offset - kctx->pattern_len;
+            }
+        }
+    }
+}
+
+static inline void compute_prefix(struct kmp_context *kctx)
+{
+    unsigned int index, match;
+
+    for (match = 0, index = 1; index < kctx->pattern_len; ++index) {
+        while (match && kctx->pattern[index] != kctx->pattern[match])
+            match = kctx->prefix_table[match - 1];
+
+        if (kctx->pattern[index] == kctx->pattern[match])
+            match++;
+
+        kctx->prefix_table[index] = match;
+    }
+}
+
+static struct ts_context *kmp_prepare(const void *pattern, size_t len,
+                                      gfp_t gfp, unsigned long flags)
+{
+    unsigned int prefix_size = sizeof(unsigned int) * len;
+    struct kmp_context *kctx;
+    unsigned int index;
+
+    kctx = kmalloc(sizeof(*kctx) + prefix_size + len, gfp);
+    if (unlikely(!kctx))
+        return ERR_PTR(-ENOMEM);
+
+    kctx->tsc.flags = flags;
+    kctx->pattern_len = len;
+    kctx->pattern = (void *)kctx + sizeof(*kctx) + prefix_size;
+
+    if (!ts_test_ignorecase(&kctx->tsc))
+        memcpy(kctx->pattern, pattern, len);
+    else for (index = 0; index < len; ++index)
+        kctx->pattern[index] = toupper(((char *)pattern)[index]);
+    compute_prefix(kctx);
+
+    return &kctx->tsc;
+}
+
+static void kmp_destroy(struct ts_context *tsc)
+{
+    struct kmp_context *kctx = ts_to_kmp(tsc);
+    kfree(kctx);
+}
+
+struct ts_algorithm kmp_algorithm = {
+    .name = "kmp",
+    .find = kmp_find,
+    .prepare = kmp_prepare,
+    .destroy = kmp_destroy,
+    .pattern_get = kmp_pattern_get,
+    .pattern_len = kmp_pattern_len,
+};
+
+static state kmp_init(void)
+{
+    return textsearch_register(&kmp_algorithm);
+}
+postcore_initcall(kmp_init);
