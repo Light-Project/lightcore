652eb5c9da6b65adb1c9b96aa891f6b6b73c30f0
John Sanpe <sanpeqf@gmail.com>
2022-11-01 18:07:40 +0800

    Function: Added a fixed matching pattern based on textsearch with grep command.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- usr/command/grep.c
+++ usr/command/grep.c
@@ -5,18 +5,22 @@
 
 #include <kshell.h>
 #include <string.h>
+#include <textsearch.h>
 #include <glob.h>
 #include <initcall.h>
 
 static void usage(struct kshell_context *ctx)
 {
-    kshell_printf(ctx, "usage: grep [option] [expression]...\n");
+    kshell_printf(ctx, "usage: grep [option]... patterns [strings]...\n");
+    kshell_printf(ctx, "\t-g  patterns are basic regular expressions\n");
+    kshell_printf(ctx, "\t-f  patterns are fixed strings\n");
     kshell_printf(ctx, "\t-h  display this message\n");
 }
 
 static state grep_main(struct kshell_context *ctx, int argc, char *argv[])
 {
-    unsigned int count, index;
+    unsigned int count, index, length;
+    bool fixed = false;
 
     for (count = 1; count < argc; ++count) {
         char *para = argv[count];
@@ -25,6 +29,14 @@ static state grep_main(struct kshell_context *ctx, int argc, char *argv[])
             break;
 
         switch (para[1]) {
+            case 'g':
+                fixed = false;
+                break;
+
+            case 'f':
+                fixed = true;
+                break;
+
             case 'h': default:
                 goto usage;
         }
@@ -33,9 +45,27 @@ static state grep_main(struct kshell_context *ctx, int argc, char *argv[])
     if (argc < count + 2)
         goto usage;
 
-    for (index = count + 1; index < argc; ++index) {
-        if (glob_match(argv[count], argv[index]))
-            kshell_printf(ctx, "%s\n", argv[index]);
+    if (fixed) {
+        struct ts_linear linear;
+        struct ts_context *tsc;
+
+        length = strlen(argv[count]);
+        tsc = textsearch_prepare("kmp", argv[count], length, GFP_KERNEL, 0);
+        if (IS_INVAL(tsc))
+            return PTR_INVAL(tsc);
+
+        for (index = count + 1; index < argc; ++index) {
+            length = strlen(argv[index]);
+            if (textsearch_linear_find(tsc, &linear, argv[index], length) != UINT_MAX)
+                kshell_printf(ctx, "%s\n", argv[index]);
+        }
+
+        textsearch_destroy(tsc);
+    } else {
+        for (index = count + 1; index < argc; ++index) {
+            if (glob_match(argv[count], argv[index]))
+                kshell_printf(ctx, "%s\n", argv[index]);
+        }
     }
 
     return -ENOERR;
