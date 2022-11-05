b638cec33afe32ebd8dc6ac2b4fa1a43db1cdb13
John Sanpe <sanpeqf@gmail.com>
2022-11-06 00:36:14 +0800

    Function: Added x86 support for system management interrupt.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ arch/x86/include/asm/smi.h
@@ -0,0 +1,22 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#ifndef _ASM_X86_SMI_H_
+#define _ASM_X86_SMI_H_
+
+#include <types.h>
+
+struct smi_regs {
+    uint32_t eax;
+    uint32_t ebx;
+    uint32_t ecx;
+    uint32_t edx;
+    uint32_t esi;
+    uint32_t edi;
+};
+
+extern void smi_trigger(struct smi_regs *regs);
+
+#endif  /* _ASM_X86_SMI_H_ */
