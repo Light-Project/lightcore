d2072bbbb150235d6abd4cccba03488be0170463
John Sanpe <sanpeqf@gmail.com>
2022-12-28 17:06:23 +0800

    Function: Added benchmark prime to selftest.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

---
+++ usr/selftest/benchmark/prime.c
@@ -0,0 +1,40 @@
+/* SPDX-License-Identifier: GPL-2.0-or-later */
+/*
+ * Copyright(c) 2022 Sanpe <sanpeqf@gmail.com>
+ */
+
+#include <initcall.h>
+#include <timekeeping.h>
+#include <math.h>
+#include <selftest.h>
+
+static state prime_testing(struct kshell_context *ctx, void *pdata)
+{
+    ktime_t start = timekeeping_get_time();
+    unsigned long value = 0, count = 0;
+
+    do {
+        value = prime_next(value);
+        count++;
+    } while (ktime_after(
+        start, ktime_sub_ms
+        (timekeeping_get_time(), 1000)
+    ));
+
+    kshell_printf(ctx, "prime benchmark: %lu/s\n", count);
+
+    return -ENOERR;
+}
+
+static struct selftest_command prime_command = {
+    .group = "benchmark",
+    .name = "prime",
+    .desc = "benchmark prime",
+    .testing = prime_testing,
+};
+
+static state selftest_prime_init(void)
+{
+    return selftest_register(&prime_command);
+}
+kshell_initcall(selftest_prime_init);
