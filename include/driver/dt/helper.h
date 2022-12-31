6eff6272fafea575c817626e239cda9bb36ddb8d
John Sanpe <sanpeqf@gmail.com>
2022-12-31 15:39:53 +0800

    Maintain: Review dt helper and simplify the code.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- include/driver/dt/helper.h
+++ include/driver/dt/helper.h
@@ -2,6 +2,27 @@
 #ifndef _DRIVER_DT_HELP_H_
 #define _DRIVER_DT_HELP_H_
 
+#define GENERIC_DT_ATTRIBUTE_VALUE_ARRAY(func, type)                                \
+static inline state dt_attribute_read_##func##_index_array                          \
+(const struct dt_node *node, const char *name, type *buff, int index, size_t min)   \
+{                                                                                   \
+    int length;                                                                     \
+    length = dt_attribute_read_##func##_index_array_range                           \
+    (node, name, buff, index, min, 0);                                              \
+    return min(length, 0);                                                          \
+}                                                                                   \
+                                                                                    \
+static inline state dt_attribute_read_##func##_array                                \
+(const struct dt_node *node, const char *name, type *buff, size_t min)              \
+{                                                                                   \
+    return dt_attribute_read_##func##_index_array(node, name, buff, 0, min);        \
+}
+
+GENERIC_DT_ATTRIBUTE_VALUE_ARRAY(u8, uint8_t)
+GENERIC_DT_ATTRIBUTE_VALUE_ARRAY(u16, uint16_t)
+GENERIC_DT_ATTRIBUTE_VALUE_ARRAY(u32, uint32_t)
+GENERIC_DT_ATTRIBUTE_VALUE_ARRAY(u64, uint64_t)
+
 static inline state dt_attribute_read_u32(const struct dt_node *node, const char *name, uint32_t *value)
 {
     return dt_attribute_read_u32_index(node, name, 0, value);
@@ -12,74 +33,14 @@ static inline state dt_attribute_read_u64(const struct dt_node *node, const char
     return dt_attribute_read_u64_index(node, name, 0, value);
 }
 
-static inline state dt_attribute_read_u8_index_array(const struct dt_node *node, const char *name, uint8_t *buff, int index, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u8_index_array_range(node, name, buff, index, min, 0);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u16_index_array(const struct dt_node *node, const char *name, uint16_t *buff, int index, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u16_index_array_range(node, name, buff, index, min, 0);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u32_index_array(const struct dt_node *node, const char *name, uint32_t *buff, int index, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u32_index_array_range(node, name, buff, index, min, 0);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u64_index_array(const struct dt_node *node, const char *name, uint64_t *buff, int index, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u64_index_array_range(node, name, buff, index, min, 0);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u8_array(const struct dt_node *node, const char *name, uint8_t *buff, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u8_index_array(node, name, buff, 0, min);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u16_array(const struct dt_node *node, const char *name, uint16_t *buff, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u16_index_array(node, name, buff, 0, min);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u32_array(const struct dt_node *node, const char *name, uint32_t *buff, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u32_index_array(node, name, buff, 0, min);
-    return min(length, 0);
-}
-
-static inline state dt_attribute_read_u64_array(const struct dt_node *node, const char *name, uint64_t *buff, size_t min)
-{
-    int length;
-    length = dt_attribute_read_u64_index_array(node, name, buff, 0, min);
-    return min(length, 0);
-}
-
 static inline state dt_attribute_read_string_index(const struct dt_node *node, const char *name, unsigned int index, const char **str)
 {
-    int length;
-    length = dt_attribute_read_string_index_array(node, name, str, index, 1);
-    return min(length, 0);
+    return dt_attribute_read_string_index_array(node, name, str, index, 1);
 }
 
 static inline state dt_attribute_read_string(const struct dt_node *node, const char *name, const char **str)
 {
-    int length;
-    length = dt_attribute_read_string_index(node, name, 0, str);
-    return min(length, 0);
+    return dt_attribute_read_string_index(node, name, 0, str);
 }
 
 static inline state dt_parse_phandle_with_args(const struct dt_node *node, const char *list_name, const char *cells_name, int index, struct dt_phandle_args *args)
