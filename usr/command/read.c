5afa6c11e9c899ecbca2662b009f654861152966
John Sanpe <sanpeqf@gmail.com>
2023-01-20 14:29:46 +0800

    Function: Added parameter to refresh timeout of read command.
    Signed-off-by: John Sanpe <sanpeqf@gmail.com>

--- usr/command/read.c
+++ usr/command/read.c
@@ -9,20 +9,26 @@
 #include <timekeeping.h>
 #include <initcall.h>
 
+static const char read_lftbl[] = {
+    ASCII_LF, ASCII_CR, 0,
+};
+
 static void usage(struct kshell_context *ctx)
 {
     kshell_puts(ctx, "usage: read [option] var\n");
     kshell_puts(ctx, "\t-s  does not echo the user's input\n");
     kshell_puts(ctx, "\t-n  returns after reading the specified number of characters\n");
     kshell_puts(ctx, "\t-t  times out after the specified time in millisecond\n");
+    kshell_puts(ctx, "\t-r  refresh timeout after input\n");
 }
 
 static state read_main(struct kshell_context *ctx, int argc, char *argv[])
 {
     unsigned int count, index, offset, nchar = 64;
+    bool refresh = false, silent = false;
     ktime_t start, timeout = 0;
-    bool silent = false;
-    char valbuf[65];
+    char *buffer;
+    state retval;
 
     for (count = 1; count < argc; ++count) {
         char *para = argv[count];
@@ -48,6 +54,10 @@ static state read_main(struct kshell_context *ctx, int argc, char *argv[])
                 timeout = ms_to_ktime(timeout);
                 break;
 
+            case 'r':
+                refresh = true;
+                break;
+
             case 'h': default:
                 goto usage;
         }
@@ -56,49 +66,55 @@ static state read_main(struct kshell_context *ctx, int argc, char *argv[])
     if (count == argc)
         goto usage;
 
+    buffer = kmalloc(nchar + 1, GFP_KERNEL);
+    if (!buffer)
+        return -ENOERR;
+
     if (timeout)
         start = timekeeping_get_time();
 
     for (offset = 0; offset < nchar; offset += index) {
-        char *lfptr, *crptr;
+        unsigned int linefeed;
 
-        index = kshell_read(ctx, valbuf + offset, nchar - offset);
+        index = kshell_read(ctx, buffer + offset, nchar - offset);
         if (index) {
             if (!silent)
-                kshell_write(ctx, valbuf + offset, index);
+                kshell_write(ctx, buffer + offset, index);
 
-            if (memchr(valbuf + offset, ASCII_ETX, index)) {
+            if (memchr(buffer + offset, ASCII_ETX, index)) {
                 ctx->canceled = true;
                 return -ECANCELED;
             }
 
-            lfptr = memchr(valbuf + offset, ASCII_LF, index);
-            crptr = memchr(valbuf + offset, ASCII_CR, index);
+            buffer[offset + index] = '\0';
+            linefeed = strcspn(buffer + offset, read_lftbl);
 
-            if (lfptr || crptr) {
-                if (crptr && crptr)
-                    lfptr = min(lfptr, crptr);
-                else
-                    lfptr += (uintptr_t)crptr;
-                offset = lfptr - valbuf;
+            if (linefeed != index) {
+                offset += linefeed;
                 break;
             }
+
+            if (timeout && refresh)
+                start = timekeeping_get_time();
         }
 
-        if (timeout && ktime_after(timekeeping_get_time(), ktime_add(start, timeout)))
-            break;
+        if (timeout && ktime_after(timekeeping_get_time(),
+            ktime_add(start, timeout))) {
+            retval = ETIMEDOUT;
+            goto finish;
+        }
 
         sched_yield();
     }
 
-    if (!offset)
-        return -ETIMEDOUT;
+    retval = kshell_setenv(ctx, argv[count], buffer, true);
 
+finish:
     if (!silent)
         kshell_putc(ctx, '\n');
 
-    valbuf[offset] = '\0';
-    return kshell_setenv(ctx, argv[count], valbuf, true);
+    kfree(buffer);
+    return retval;
 
 usage:
     usage(ctx);
